24e00b42fd906c9898f4881365e9d04879a1bf6a
Adrien KISSIE <fredkiss3@gmail.com>
2024-02-20 21:39:57 +0100

    🔥 remove register form as it is not needed yet

--- backend/zane_api/forms.py
+++ backend/zane_api/forms.py
@@ -4,25 +4,3 @@ import django.forms as forms
 class PasswordLoginForm(forms.Form):
     username = forms.CharField(required=True, min_length=1, strip=True, max_length=255)
     password = forms.CharField(required=True, max_length=255, min_length=1)
-
-
-PASSWORD_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-
-
-class PasswordRegisterForm(forms.Form):
-    username = forms.CharField(
-        required=True,
-        min_length=1,
-        strip=True,
-        max_length=255,
-    )
-    password = forms.RegexField(
-        required=True,
-        regex=PASSWORD_REGEX,
-        error_messages={
-            "invalid": "Please a valid password : ensures at least one lowercase letter exists,"
-            " one uppercase, one digit, one special character exists"
-            " and that the length is at least 8 characters"
-        },
-        max_length=255,
-    )
