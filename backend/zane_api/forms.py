91e77a91fb0601060229b808420a3fce933b2144
Adrien KISSIE <fredkiss3@gmail.com>
2024-02-20 19:43:16 +0100

    ♿️ better message for password register form

--- backend/zane_api/forms.py
+++ backend/zane_api/forms.py
@@ -19,6 +19,10 @@ class PasswordRegisterForm(forms.Form):
     password = forms.RegexField(
         required=True,
         regex=PASSWORD_REGEX,
-        error_messages={"invalid": "Please a valid password"},
+        error_messages={
+            "invalid": "Please a valid password : ensures at least one lowercase letter exists,"
+            " one uppercase, one digit, one special character exists"
+            " and that the length is at least 8 characters"
+        },
         max_length=255,
     )
