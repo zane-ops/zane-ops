31714ee19f47b4501b070f767310298e67b2bf4f
Adrien KISSIE <fredkiss3@gmail.com>
2024-03-09 05:03:30 +0100

    :construction: working on it...

--- backend/zane_api/tests/volume.py
+++ backend/zane_api/tests/volume.py
@@ -3,11 +3,17 @@ from unittest.mock import patch, Mock, MagicMock
 
 
 class FakeDockerClient:
-    def __init__(self):
+    def __init__(self, raise_error: bool = False):
         self.volumes = MagicMock()
+        self.raise_error = raise_error
+
+        self.volumes.create = self.volumes_create
+
+    def volumes_create(self, **kwargs):
+        pass
 
 
-@patch("zane_api.views.docker.DockerService._get_client", wraps=FakeDockerClient)
 class DockerVolumeTests(TestCase):
+    @patch("zane_api.services.get_docker_client", return_value=FakeDockerClient())
     def test_create_volume(self, mock_fake_docker: Mock):
         pass
