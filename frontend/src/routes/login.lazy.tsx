50b1afa91e474a9e4f361e1cacd49ab41aa65d25
Mohamed CHERIF <mocherifh0@gmail.com>
2024-03-13 20:26:21 +0000

    feat: errors showing (#45)
    * feat: errors showing
    
    * feat: radix
    
    * fix: fixes:
    
    * fix: type
    
    * fix: fixes
    
    * fix: missingValue
    
    * fix: format
    
    * fix: Alert
    
    * fix: ??
    
    * fix: fix
    
    * fix: remvoed git add .
    
    * fix: fix

--- frontend/src/routes/login.lazy.tsx
+++ frontend/src/routes/login.lazy.tsx
@@ -1,9 +1,14 @@
+import * as Form from "@radix-ui/react-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 import { RequestInput, apiClient } from "~/api/client";
 import { Input } from "~/components/ui/input";
 import whiteLogo from "/logo/Zane-Ops-logo-white-text.svg";
 import logoSymbol from "/logo/ZaneOps-SYMBOL-BLACK.svg";
+
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 
 export const Route = createLazyFileRoute("/login")({
   component: Login
@@ -52,52 +57,74 @@ function Login() {
           </p>
         </div>
 
-        <form
+        <Form.Root
           className="p-7 md:px-32 md:w-[50%]  flex flex-col w-full"
-          action={(formData) =>
-            mutate({
-              username: formData.get("username")!.toString(),
-              password: formData.get("password")!.toString()
-            })
-          }
+          asChild
         >
-          <h1 className="md:text-2xl text-3xl md:text-left text-center font-bold my-3">
-            Log in
-          </h1>
-          <div className="card flex flex-col gap-3">
-            {data?.errors?.root && (
-              <div style={{ color: "red" }}>{data.errors.root}</div>
-            )}
-            <div>
-              <Input
-                placeholder="username"
-                aria-label="username"
-                name="username"
-                type="text"
-              />
-              {!!data?.errors?.username && (
-                <p style={{ color: "red" }}>{data.errors.username}</p>
+          <form
+            action={(formData) =>
+              mutate({
+                username: formData.get("username")!.toString(),
+                password: formData.get("password")!.toString()
+              })
+            }
+          >
+            <h1 className="md:text-2xl text-3xl md:text-left text-center font-bold my-3">
+              Log in
+            </h1>
+            <div className="card flex flex-col gap-3">
+              {data?.errors?.root && (
+                <Alert variant="destructive">
+                  <AlertCircle className="h-4 w-4" />
+                  <AlertTitle>Error</AlertTitle>
+                  <AlertDescription>{data.errors.root}</AlertDescription>
+                </Alert>
               )}
+
+              <Form.Field className="my-2 flex flex-col gap-1" name="username">
+                <Form.Label className="">Username</Form.Label>
+                <Form.Control asChild>
+                  <Input
+                    placeholder="ex: JohnDoe"
+                    aria-label="username"
+                    name="username"
+                    type="text"
+                  />
+                </Form.Control>
+                {data?.errors?.username && (
+                  <Form.Message className="text-red-500 text-sm">
+                    {data.errors.username}
+                  </Form.Message>
+                )}
+              </Form.Field>
+
+              <Form.Field className="my-2 flex flex-col gap-1" name="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control asChild>
+                  <Input
+                    aria-label="password"
+                    type="password"
+                    name="password"
+                  />
+                </Form.Control>
+                {data?.errors?.password && (
+                  <Form.Message className="text-red-500 text-sm">
+                    {data.errors.password}
+                  </Form.Message>
+                )}
+              </Form.Field>
+
+              <Form.Submit asChild>
+                <button
+                  className="bg-slate-900  w-full p-3 text-white rounded-lg"
+                  disabled={isPending}
+                >
+                  {isPending ? "Submitting..." : "Submit"}
+                </button>
+              </Form.Submit>
             </div>
-            <div>
-              <Input
-                aria-label="password"
-                placeholder="password"
-                type="password"
-                name="password"
-              />
-              {!!data?.errors?.password && (
-                <p style={{ color: "red" }}>{data.errors.password}</p>
-              )}
-            </div>
-            <button
-              className="bg-slate-900  w-full p-3 text-white rounded-lg"
-              disabled={isPending}
-            >
-              {isPending ? "Submitting..." : "Submit"}
-            </button>
-          </div>
-        </form>
+          </form>
+        </Form.Root>
       </div>
     </>
   );
