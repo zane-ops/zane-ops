30fc210ad9882b4c31ca209415be9e2addc6f887
Adrien KISSIE <fredkiss3@gmail.com>
2024-03-17 07:13:09 +0100

    :alembic: Using docker.events is the way

--- backend/docker_service_testing.py
+++ backend/docker_service_testing.py
@@ -1,58 +1,46 @@
-import docker
-
+import docker.errors
 from docker.types import EndpointSpec, RestartPolicy, UpdateConfig
 
 if __name__ == '__main__':
     client = docker.from_env()
-    endpoint_spec = EndpointSpec(ports={
-        6382: 6379
-    })
-    client.services.create(
-        image='redis:latest',
-        name="cache_db",
-        mounts=['redis_data_volume:/data:rw'],
-        env=[
-            "REDIS_PASSWORD=strongPassword123"
-        ],
-        networks=['zane-out'],
-        endpoint_spec=endpoint_spec,
-        restart_policy=RestartPolicy(
-            condition="on-failure",
-            max_attempts=3,
-            delay=5,
-        ),
-        update_config=UpdateConfig(
-            parallelism=1,
-            delay=5,
-            monitor=10,
-            order="start-first",
-            failure_action="rollback"
-        ),
-        # command="psql",
-        # command="redis-server --requirepass ${REDIS_PASSWORD}"
-        # labels={},
-    )
-    # client = docker.APIClient()
-    #
-    # volume = Mount(target="/data", source="redis_data_volume")
-    # container_spec = ContainerSpec(
-    #     image='redis:latest',
-    #     mounts=[volume]
-    #     # env={
-    #     #     "REDIS_PASSWORD": "strongPassword123"
-    #     # },
-    #     # command="redis-server --requirepass ${REDIS_PASSWORD}"
-    # )
-    #
-    # task_tmpl = TaskTemplate(container_spec)
-    # endpoint_spec = EndpointSpec(ports={
-    #     6382: 6379
-    # })
-    # service_id = client.create_service(
-    #     task_tmpl,
-    #     name='cache_db',
-    #     endpoint_spec=endpoint_spec,
-    #     networks=['zane-out'],
-    # )
+    endpoint_spec = EndpointSpec(ports={6382: 6379})
+    try:
+        service = client.services.get("cache_db")
+    except docker.errors.NotFound:
+        service = client.services.create(
+            image='redis:latest',
+            name="cache_db",
+            mounts=['redis_data_volume:/data:rw'],
+            env=["REDIS_PASSWORD=strongPassword123"],
+            networks=['zane-out'],
+            endpoint_spec=endpoint_spec,
+            restart_policy=RestartPolicy(
+                condition="on-failure",
+                max_attempts=3,
+                delay=5,
+            ),
+            update_config=UpdateConfig(
+                parallelism=1,
+                delay=5,
+                monitor=10,
+                order="start-first",
+                failure_action="rollback"
+            ),
+            # command="psql",
+            # command="redis-server --requirepass ${REDIS_PASSWORD}"
+            # labels={},
+        )
 
-    # client.remove_service(service_id)
+    result = service.scale(replicas=1)
+    for event in client.events(decode=True, filters={'service': 'cache_db'}):
+        print(event)
+        if event['status'] == 'start' and event['Type'] == 'container':
+            break
+
+    # service.update()
+    # # print(dict(result=result))
+    # for key, val in service.attrs.items():
+    #     print(f"{key}={val}")
+    # for log in service.logs(follow=True):
+    #     print(dict(log=log))
+    # service.scale(replicas=1)
