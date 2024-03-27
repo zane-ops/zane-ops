0430374d4b847447b6bc635ed8dbe5ad3878b0f0
Adrien KISSIE <fredkiss3@gmail.com>
2024-03-27 06:35:46 +0100

    :adhesive_bandage: Reset DB

--- reset-db.sh
+++ reset-db.sh
@@ -3,15 +3,34 @@ source ./backend/venv/bin/activate && echo yes | python ./backend/manage.py flus
 
 echo "Scaling down all zane-ops services..."
 services=$(docker service ls --filter label=zane-managed=true --format "{{.Name}}")
-for service in $services; do
+for service in $services
+do
   docker service scale $service=0
 done
+echo "Waiting for 5 seconds for the services to to finish scaling down..."
+sleep 5
 
 echo "Deleting services..."
 docker service rm $(docker service ls -q --filter label=zane-managed=true)  2>/dev/null
 
 echo "Deleting volumes..."
 docker volume rm $(docker volume ls -q --filter label=zane-managed=true) 2>/dev/null
+
+echo "Detaching networks from the proxy..."
+
+zane_networks_ids=$(docker network ls -q --filter label=zane-managed=true --format '{{.ID}}')
+network_options=""
+while read -r net_id;
+do
+  network_options+=" --network-rm $net_id"
+done <<< "$zane_networks_ids"
+
+service_name="zane_zane-proxy"
+echo "docker service update $network_options zane_zane-proxy"
+if [ -n "$network_options" ]; then
+    # Update the service with all networks
+    docker service update $network_options $service_name
+fi
 
 echo "Deleting networks..."
 docker network rm $(docker network ls -q --filter label=zane-managed=true) 2>/dev/null
