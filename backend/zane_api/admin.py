44dd651efc9c3dde037a16b0ef546f20b06ab456
Adrien KISSIE <fredkiss3@gmail.com>
2024-04-25 10:40:49 +0200

    Revert "🔥 remove admin because unused"
    This reverts commit e217c72db6f4fc18ec66ac065e8bc1b7fb654307.

--- backend/zane_api/admin.py
+++ backend/zane_api/admin.py
@@ -1,1 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Project, DockerRegistryService, GitRepositoryService
+
+
+# Register your models here.
+@admin.register(Project)
+class Project(admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
+@admin.register(DockerRegistryService)
+class DockerRegistryService(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GitRepositoryService)
+class GitRepositoryService(admin.ModelAdmin):
+    pass
