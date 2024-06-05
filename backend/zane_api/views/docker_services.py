6ec13c625b7079ca46654688841d0c343a13d564
Adrien KISSIE <fredkiss3@gmail.com>
2024-06-05 03:33:40 +0200

    :recycle: Do not run deploy task on service creation

--- backend/zane_api/views/docker_services.py
+++ backend/zane_api/views/docker_services.py
@@ -41,11 +41,6 @@ from ..models import (
     ArchivedProject,
     ArchivedDockerService,
     DockerDeploymentChange,
-    HealthCheck,
-    Volume,
-    PortConfiguration,
-    URL,
-    DockerEnvVariable,
 )
 from ..serializers import (
     DockerServiceDeploymentSerializer,
@@ -59,10 +54,8 @@ from ..serializers import (
 )
 from ..tasks import (
     delete_resources_for_docker_service,
-    deploy_docker_service,
     deploy_docker_service_with_changes,
 )
-from ..utils import strip_slash_if_exists
 
 
 class CreateDockerServiceAPIView(APIView):
@@ -95,30 +88,16 @@ class CreateDockerServiceAPIView(APIView):
                 Faker.seed(time.monotonic())
                 service_slug = data.get("slug", fake.slug()).lower()
                 try:
-                    healthcheck = data.get("healthcheck")
                     service = DockerRegistryService.objects.create(
                         slug=service_slug,
                         project=project,
-                        image=data["image"],
-                        command=data.get("command"),
-                        credentials=docker_credentials,
-                        healthcheck=(
-                            HealthCheck.objects.create(
-                                type=healthcheck["type"],
-                                value=healthcheck["value"],
-                                timeout_seconds=healthcheck["timeout_seconds"],
-                                interval_seconds=healthcheck["interval_seconds"],
-                            )
-                            if healthcheck is not None
-                            else None
-                        ),
                     )
 
                     service.network_alias = f"{service.slug}-{service.unprefixed_id}"
 
                     initial_changes = [
                         DockerDeploymentChange(
-                            field="image",
+                            field=DockerDeploymentChange.ChangeField.IMAGE,
                             new_value=data["image"],
                             type=DockerDeploymentChange.ChangeType.UPDATE,
                             service=service,
@@ -128,7 +107,7 @@ class CreateDockerServiceAPIView(APIView):
                     if docker_credentials is not None:
                         initial_changes.append(
                             DockerDeploymentChange(
-                                field="credentials",
+                                field=DockerDeploymentChange.ChangeField.CREDENTIALS,
                                 new_value=docker_credentials,
                                 type=DockerDeploymentChange.ChangeType.UPDATE,
                                 service=service,
@@ -140,134 +119,6 @@ class CreateDockerServiceAPIView(APIView):
                     raise ResourceConflict(
                         detail=f"A service with the slug `{service_slug}` already exists."
                     )
-
-                # Create volumes if exists
-                volumes_request = data.get("volumes", [])
-                created_volumes = Volume.objects.bulk_create(
-                    [
-                        Volume(
-                            name=volume.get("name", fake.slug().lower()),
-                            container_path=volume["container_path"],
-                            host_path=volume.get("host_path"),
-                            mode=volume.get("mode"),
-                        )
-                        for volume in volumes_request
-                    ]
-                )
-
-                service.volumes.add(*created_volumes)
-
-                # create ports configuration
-                service_urls_from_request = data.get("urls", [])
-                ports_from_request = data.get("ports", [])
-                http_ports = [80, 443]
-
-                if len(service_urls_from_request) > 0:
-                    has_at_least_one_http_port = False
-                    for port in ports_from_request:
-                        if port["host"] in http_ports:
-                            has_at_least_one_http_port = True
-                            break
-
-                    if not has_at_least_one_http_port:
-                        ports_from_request.append(
-                            {
-                                "host": 80,
-                                "forwarded": 80,
-                            }
-                        )
-
-                created_ports = PortConfiguration.objects.bulk_create(
-                    [
-                        PortConfiguration(
-                            host=(
-                                port["host"] if port["host"] not in http_ports else None
-                            ),
-                            forwarded=port["forwarded"],
-                        )
-                        for port in ports_from_request
-                    ]
-                )
-
-                service.ports.add(*created_ports)
-
-                # Create urls to route the service to
-                can_create_urls = len(service_urls_from_request) > 0
-                if not can_create_urls:
-                    for port in ports_from_request:
-                        public_port = port["host"]
-                        if public_port in http_ports:
-                            can_create_urls = True
-                            break
-
-                if can_create_urls:
-                    if len(service_urls_from_request) == 0:
-                        existing_urls = URL.objects.filter(
-                            domain=f"{project.slug}-{service_slug}.{settings.ROOT_DOMAIN}",
-                            base_path="/",
-                        ).first()
-                        if existing_urls is None:
-                            default_url = URL.create_default_url(service=service)
-                        else:
-                            default_url = URL.objects.create(
-                                domain=f"{project.slug}-{service_slug}-{fake.slug()}.{settings.ROOT_DOMAIN}",
-                                base_path="/",
-                            )
-                        service.urls.add(default_url)
-                    else:
-                        urls_to_create: list[URL] = []
-
-                        for url in service_urls_from_request:
-                            base_path = (
-                                "/"
-                                if url["base_path"] == "/"
-                                else strip_slash_if_exists(
-                                    url["base_path"],
-                                    strip_end=True,
-                                    strip_start=False,
-                                )
-                            )
-                            urls_to_create.append(
-                                URL(
-                                    domain=url["domain"],
-                                    base_path=base_path,
-                                    strip_prefix=url["strip_prefix"],
-                                )
-                            )
-
-                        created_urls = URL.objects.bulk_create(urls_to_create)
-                        service.urls.add(*created_urls)
-
-                # Create first deployment
-                first_deployment = DockerDeployment.objects.create(
-                    service=service, is_current_production=True
-                )
-                if len(service.urls.all()) > 0:
-                    first_deployment.url = f"{project.slug}-{service_slug}-docker-{first_deployment.unprefixed_hash}.{settings.ROOT_DOMAIN}"
-                    first_deployment.save()
-
-                # Create envs if exists
-                envs_from_request: dict[str, str] = data.get("env", {})
-
-                DockerEnvVariable.objects.bulk_create(
-                    [
-                        DockerEnvVariable(key=key, value=value, service=service)
-                        for key, value in envs_from_request.items()
-                    ]
-                )
-
-                token = Token.objects.get(user=request.user)
-                # Run celery deployment task
-                transaction.on_commit(
-                    lambda: deploy_docker_service.apply_async(
-                        kwargs=dict(
-                            deployment_hash=first_deployment.hash,
-                            service_id=service.id,
-                            auth_token=token.key,
-                        ),
-                        task_id=first_deployment.task_id,
-                    )
-                )
 
                 response = DockerServiceSerializer(service)
                 return Response(response.data, status=status.HTTP_201_CREATED)
