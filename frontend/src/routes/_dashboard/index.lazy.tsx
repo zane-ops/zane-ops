3c421581253df82ba513cb0c394db1cfa2897beb
mo-cherif <mocherifh0@gmail.com>
2024-06-18 23:29:33 +0100

    fix: fix

--- frontend/src/routes/_dashboard/index.lazy.tsx
+++ frontend/src/routes/_dashboard/index.lazy.tsx
@@ -1,7 +1,7 @@
 import {
   createFileRoute,
   createLazyFileRoute,
-  useNavigate,
+  useNavigate
 } from "@tanstack/react-router";
 import {
   ArrowDown,
@@ -10,7 +10,7 @@ import {
   Rocket,
   Search,
   Settings,
-  Trash,
+  Trash
 } from "lucide-react";
 import { withAuthRedirect } from "~/components/helper/auth-redirect";
 import { useAuthUser } from "~/components/helper/use-auth-user";
@@ -22,7 +22,7 @@ import {
   MenubarContent,
   MenubarContentItem,
   MenubarMenu,
-  MenubarTrigger,
+  MenubarTrigger
 } from "~/components/ui/menubar";
 
 import React from "react";
@@ -37,7 +37,7 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
+  TableRow
 } from "~/components/ui/table";
 import { useProjectList } from "~/lib/hooks/use-project-list";
 import { formattedDate } from "~/utils";
@@ -46,12 +46,12 @@ import { z } from "zod";
 import { Button } from "~/components/ui/button";
 
 const projectSearchSchema = z.object({
-  slug: z.string().catch(""),
+  slug: z.string().catch("")
 });
 
 export const Route = createFileRoute("/_dashboard/")({
   validateSearch: (search) => projectSearchSchema.parse(search),
-  component: withAuthRedirect(AuthedView),
+  component: withAuthRedirect(AuthedView)
 });
 
 function AuthedView() {
