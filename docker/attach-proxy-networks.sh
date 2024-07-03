56eefc7666b2a78cd32804b19ca4c15692ce1491
Adrien KISSIE <fredkiss3@gmail.com>
2024-07-03 06:19:03 +0200

    Chore: Updated setup scripts and contributing docs (#140)
    * :hammer: Do not keep snapshot of the alive services in files
    
    * :hammer: Update setup scripts
    
    * :memo: Update contributing docs

--- docker/attach-proxy-networks.sh
+++ docker/attach-proxy-networks.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 echo "Attaching saved networks to the proxy..."
-# Service name or ID
-service_name="zane_zane-proxy"
-
-# File containing the new network IDs
-file=".proxy_attached_networks"
-
-if [ ! -f $file ]; then
-    echo "No saved network found."
-    exit 0
-fi
-
-# Read and prepare network IDs from the file
-while read p; do
-  net_id=$(echo $p | sed 's/"//g')
-  if [ -n "$net_id" ]; then
-   docker service update --network-add $net_id $service_name
+network_ids=$(docker network ls --filter "label=zane-managed=true" -q)
+for network_id in $network_ids;
+do
+  if [ -n "$network_id" ]; then
+    docker service update --network-add "$network_id" zane_zane-proxy
   fi
-done <$file
+done
 
