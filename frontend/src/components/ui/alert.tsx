7e27b989b8dcef179e27bdd8ab472c632f112fd6
Adrien KISSIE <fredkiss3@gmail.com>
2024-07-29 03:29:54 +0200

    💄 change make alert info color legible in light mode

--- frontend/src/components/ui/alert.tsx
+++ frontend/src/components/ui/alert.tsx
@@ -4,13 +4,13 @@ import * as React from "react";
 import { cn } from "~/lib/utils";
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4",
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-5",
   {
     variants: {
       variant: {
         default: "bg-background text-foreground",
         success: "border-green-400 text-green-400",
-        info: "border-secondary text-secondary",
+        info: "border-link text-link",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive"
       }
