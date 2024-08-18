5ed3ae2695bcf0da7b9ed20572adba5a06901dcf
mo-cherif <mocherifh0@gmail.com>
2024-08-18 17:14:32 +0100

    fix: removed unsued icons

--- frontend/src/components/ui/mutiple-select.tsx
+++ frontend/src/components/ui/mutiple-select.tsx
@@ -1,11 +1,5 @@
 import { type VariantProps, cva } from "class-variance-authority";
-import {
-  CheckIcon,
-  ChevronDown,
-  WandSparkles,
-  XCircle,
-  XIcon
-} from "lucide-react";
+import { CheckIcon, ChevronDown } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "../../lib/utils";
