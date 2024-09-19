645eba8734f713889fb35bf399818eac5682209d
Adrien KISSIE <fredkiss3@gmail.com>
2024-09-19 19:02:39 +0200

    🗃️ make URL domain mandatory

--- backend/zane_api/models/base.py
+++ backend/zane_api/models/base.py
@@ -54,9 +54,7 @@ class URL(models.Model):
         primary_key=True,
         prefix=ID_PREFIX,
     )
-    domain = models.CharField(
-        max_length=1000, null=True, blank=True, validators=[validate_url_domain]
-    )
+    domain = models.CharField(max_length=1000, validators=[validate_url_domain])
     base_path = models.CharField(default="/", validators=[validate_url_path])
     strip_prefix = models.BooleanField(default=True)
 
