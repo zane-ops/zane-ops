5cdeba347c5e66b1a00f7d0ab887eb3a190ef3a7
Adrien KISSIE <fredkiss3@gmail.com>
2024-09-21 17:51:07 +0200

    fix: mark stuck deployments as `removed` when a new deployment is succesfull (#227)
    ## Description
    
    This PR fixes a bug on production where deployments which were stuck in
    the deploying step and weren't marked removed. This is because of a Bug
    that probably happenned in the temporal workflow step keeping them
    stuck.
    
    ### ⚠️ If you modify backend code, be sure to run these commands : 
    
    ```bash
    cd backend
    pnpm run openapi # will generate OpenAPI schema at `/openapi/schema.yaml`
    pnpm run freeze # will update the `requirements.txt` file if you added new packages
    ```
    
    ### ⚠️ If you modify frontend code, be sure to run these commands : 
    
    ```bash
    pnpm run format # format the files using biome
    ```
    
    
    ## Type of Change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)

--- backend/zane_api/temporal/activities.py
+++ backend/zane_api/temporal/activities.py
@@ -897,6 +897,20 @@ class DockerSwarmActivities:
                 await deployment.service.deployments.filter(
                     ~Q(hash=healthcheck_result.deployment_hash)
                 ).aupdate(is_current_production=False)
+                await deployment.service.deployments.filter(
+                    ~Q(hash=healthcheck_result.deployment_hash)
+                    & Q(
+                        status__in=[
+                            DockerDeployment.DeploymentStatus.PREPARING,
+                            DockerDeployment.DeploymentStatus.STARTING,
+                            DockerDeployment.DeploymentStatus.RESTARTING,
+                        ]
+                    )
+                    & Q(finished_at__isnull=True),
+                ).aupdate(
+                    finished_at=timezone.now(),
+                    status=DockerDeployment.DeploymentStatus.REMOVED,
+                )
         except DockerDeployment.DoesNotExist:
             raise ApplicationError(
                 "Cannot save a non existent deployment.",
