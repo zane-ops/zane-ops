629d2ef8cf328b46cf465953c9a4b70f8d9ccdea
Adrien KISSIE <fredkiss3@gmail.com>
2024-09-27 23:18:51 +0200

    🎨 biome format

--- frontend/src/routes/_dashboard/project_/$project_slug.services.docker.$service_slug/settings.lazy.tsx
+++ frontend/src/routes/_dashboard/project_/$project_slug.services.docker.$service_slug/settings.lazy.tsx
@@ -1,5 +1,12 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/_dashboard/project/$project_slug/services/docker/$service_slug/settings')({
-  component: () => <div>Hello /_dashboard/project/$project_slug/services/docker/$service_slug/settings!</div>
-})
+export const Route = createLazyFileRoute(
+  "/_dashboard/project/$project_slug/services/docker/$service_slug/settings"
+)({
+  component: () => (
+    <div>
+      Hello
+      /_dashboard/project/$project_slug/services/docker/$service_slug/settings!
+    </div>
+  )
+});
