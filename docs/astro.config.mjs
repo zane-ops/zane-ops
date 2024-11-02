7b530b279037ed5f13a70983e8a8ed6f0440bdeb
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-02 07:59:19 +0100

    ♻️ move zaneops docs to the root

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zane.fredkiss.dev",
-  base: "/docs",
   devToolbar: {
     enabled: false
   },
