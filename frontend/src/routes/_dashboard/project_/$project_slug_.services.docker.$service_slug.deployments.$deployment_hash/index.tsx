c026097228ba626c55cbb81d1435a1b01bdb067a
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-10 23:56:15 +0100

    🚧 we can now see logs

--- frontend/src/routes/_dashboard/project_/$project_slug_.services.docker.$service_slug.deployments.$deployment_hash/index.tsx
+++ frontend/src/routes/_dashboard/project_/$project_slug_.services.docker.$service_slug.deployments.$deployment_hash/index.tsx
@@ -1,46 +1,17 @@
-import * as Form from "@radix-ui/react-form";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Link } from "@tanstack/react-router";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { type VariantProps, cva } from "class-variance-authority";
-import { Command as CommandPrimitive } from "cmdk";
-import {
-  CheckIcon,
-  ChevronDownIcon,
-  LoaderIcon,
-  SearchIcon,
-  XIcon
-} from "lucide-react";
+import { LoaderIcon, SearchIcon, XIcon } from "lucide-react";
 import * as React from "react";
 import { type DateRange } from "react-day-picker";
 import { useDebounce } from "use-debounce";
 import { DateRangeWithShortcuts } from "~/components/date-range-with-shortcuts";
 import { withAuthRedirect } from "~/components/helper/auth-redirect";
 import { Loader } from "~/components/loader";
+import { MultiSelect } from "~/components/multi-select";
 import { Button } from "~/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator
-} from "~/components/ui/command";
+
 import { Input } from "~/components/ui/input";
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger
-} from "~/components/ui/popover";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger
-} from "~/components/ui/tooltip";
 import {
   type DeploymentLogFitlers,
   LOG_LEVELS,
@@ -50,6 +21,7 @@ import {
 } from "~/lib/queries";
 import type { Writeable } from "~/lib/types";
 import { cn } from "~/lib/utils";
+
 import { isEmptyObject } from "~/utils";
 
 export const Route = createFileRoute(
@@ -64,17 +36,18 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
   const searchParams = Route.useSearch();
   const navigate = useNavigate();
 
-  const [debouncedQuery] = useDebounce(searchParams.query ?? "", 300);
+  const [debouncedSearchQuery] = useDebounce(searchParams.content ?? "", 300);
+  const inputRef = React.useRef<React.ElementRef<"input">>(null);
 
   const filters = {
     page: searchParams.page ?? 1,
-    per_page: searchParams.per_page ?? 10,
+    per_page: searchParams.per_page ?? 50,
     time_after: searchParams.time_after,
     time_before: searchParams.time_before,
     source:
       searchParams.source ?? (LOG_SOURCES as Writeable<typeof LOG_SOURCES>),
     level: searchParams.level ?? (LOG_LEVELS as Writeable<typeof LOG_LEVELS>),
-    query: debouncedQuery
+    content: debouncedSearchQuery
   } satisfies DeploymentLogFitlers;
 
   const logsQuery = useInfiniteQuery(
@@ -85,6 +58,26 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
       filters
     })
   );
+
+  const logs = React.useMemo(() => {
+    return (
+      logsQuery.data?.pages
+        .flat()
+        .map((item) => item.results)
+        .flat() ?? []
+    );
+  }, [logsQuery.data]);
+
+  const clearFilters = React.useCallback(() => {
+    navigate({
+      to: "./",
+      replace: true
+    });
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  }, [navigate]);
 
   if (logsQuery.isLoading) {
     return <Loader className="h-[50vh]" />;
@@ -135,17 +128,18 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
             <Input
               className="px-14 w-full text-sm  bg-muted/40 dark:bg-card/30"
               placeholder="Search for log contents"
-              name="query"
-              defaultValue={searchParams.query}
+              name="content"
+              defaultValue={searchParams.content}
+              ref={inputRef}
               onKeyUp={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
                 const newQuery = e.currentTarget.value;
-                if (newQuery !== (searchParams.query ?? "")) {
+                if (newQuery !== (searchParams.content ?? "")) {
                   navigate({
                     search: {
                       ...filters,
-                      query: e.currentTarget.value
+                      content: e.currentTarget.value
                     },
                     replace: true
                   });
@@ -155,7 +149,7 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
           </div>
 
           <div className="flex-shrink-0 flex items-center gap-1.5 order-1 lg:order-last">
-            <SimpleMultiSelect
+            <MultiSelect
               value={filters.level}
               options={LOG_LEVELS as Writeable<typeof LOG_LEVELS>}
               onValueChange={(newVal) => {
@@ -170,7 +164,7 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
               placeholder="log levels"
             />
 
-            <SimpleMultiSelect
+            <MultiSelect
               value={filters.source}
               options={LOG_SOURCES as Writeable<typeof LOG_SOURCES>}
               onValueChange={(newVal) => {
@@ -191,212 +185,67 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
           <Button
             variant="outline"
             className="inline-flex w-min gap-1"
-            onClick={() =>
-              navigate({
-                to: "./"
-              })
-            }
+            onClick={clearFilters}
           >
             <XIcon size={15} />
             <span>Reset filters</span>
           </Button>
         )}
 
-        <div className="rounded-md px-4 pb-2  overflow-y-auto bg-muted/25 dark:bg-card h-full w-full">
-          <pre
-            id="logContent"
-            className="text-base whitespace-no-wrap overflow-x-scroll font-mono pt-2 relative h-full"
-          >
-            <div className="text-center items-center flex flex-col text-gray-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-              <h3 className="text-lg font-semibold">No logs to show</h3>
-              <small className="inline-block max-w-lg text-balance">
-                New log entries that match your search parameters will appear
-                here.
-              </small>
-              <button className="text-sm underline">Clear filters</button>
-            </div>
-          </pre>
-        </div>
+        <pre
+          id="logContent"
+          className="text-xs whitespace-no-wrap font-mono pt-2 pb-10 relative h-full rounded-md w-full bg-muted/25 dark:bg-card overflow-y-auto"
+        >
+          {logs.length === 0 &&
+            (logsQuery.isFetching ? (
+              <div className="text-sm text-center items-center flex gap-2 text-gray-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <LoaderIcon size={15} className="animate-spin" />
+                <p>Fetching logs...</p>
+              </div>
+            ) : isEmptyObject(searchParams) ? (
+              <div className="text-sm text-center items-center flex flex-col text-gray-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h3 className="text-base font-semibold">No logs yet</h3>
+                <p className="inline-block max-w-lg text-balance ">
+                  New log entries will appear here.
+                </p>
+              </div>
+            ) : (
+              <div className="text-sm px-2 gap-1.5 text-center items-center flex flex-col text-gray-500 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h3 className="text-base font-semibold text-balance w-full">
+                  No logs maching the selected filters
+                </h3>
+                <p className="inline-block max-w-lg text-balance">
+                  New log entries that match your search parameters will appear
+                  here.
+                </p>
+                <button className="text-sm underline" onClick={clearFilters}>
+                  Clear filters
+                </button>
+              </div>
+            ))}
+
+          {logs.length > 0 &&
+            logs.map((log) => (
+              <div
+                key={log.id}
+                className={cn(
+                  "flex gap-2 px-2",
+                  log.level === "ERROR" && "bg-red-400/20"
+                )}
+              >
+                <span className="text-grey">
+                  [{new Date(log.time).toLocaleString()}]
+                </span>
+                <pre className="text-wrap  break-all">
+                  {log.content as string}
+                </pre>
+                {/* {!!searchValue
+                          ? getHighlightedText(log.content, searchValue)
+                          : colorLogs(log.content)} */}
+              </div>
+            ))}
+        </pre>
       </div>
     </div>
   );
 }
-
-/**
- * Variants for the multi-select component to handle different styles.
- * Uses class-variance-authority (cva) to define different styles based on "variant" prop.
- */
-const multiSelectVariants = cva(
-  "m-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-foreground/10 text-foreground bg-card hover:bg-card/80",
-        secondary:
-          "border-foreground/10 bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        inverted: "inverted"
-      }
-    },
-    defaultVariants: {
-      variant: "default"
-    }
-  }
-);
-
-interface MultiSelectProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof multiSelectVariants> {
-  /**
-   * An array of option objects to be displayed in the multi-select component.
-   * Each option object has a label, value, and an optional icon.
-   */
-  options: string[];
-
-  /**
-   * Callback function triggered when the selected values change.
-   * Receives an array of the new selected values.
-   */
-  onValueChange: (value: string[]) => void;
-
-  /**
-   * Placeholder text to be displayed when no values are selected.
-   * Optional, defaults to "Select options".
-   */
-  placeholder?: string;
-
-  /**
-   * Animation duration in seconds for the visual effects (e.g., bouncing badges).
-   * Optional, defaults to 0 (no animation).
-   */
-  animation?: number;
-
-  /**
-   * Maximum number of items to display. Extra selected items will be summarized.
-   * Optional, defaults to 3.
-   */
-  maxCount?: number;
-
-  /**
-   * The modality of the popover. When set to true, interaction with outside elements
-   * will be disabled and only popover content will be visible to screen readers.
-   * Optional, defaults to false.
-   */
-  modalPopover?: boolean;
-
-  /**
-   * If true, renders the multi-select component as a child of another component.
-   * Optional, defaults to false.
-   */
-  asChild?: boolean;
-
-  /**
-   * Additional class names to apply custom styles to the multi-select component.
-   * Optional, can be used to add custom styles.
-   */
-  className?: string;
-  value: string[];
-}
-const SimpleMultiSelect = React.forwardRef<HTMLButtonElement, MultiSelectProps>(
-  (
-    {
-      options,
-      onValueChange,
-      variant,
-      value = [],
-      placeholder = "Select options",
-      animation = 0,
-      maxCount = 3,
-      modalPopover = false,
-      asChild = false,
-      className,
-      ...props
-    },
-    ref
-  ) => {
-    const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
-
-    const toggleOption = (option: string) => {
-      const newSelectedValues = value.includes(option)
-        ? value.filter((v) => v !== option)
-        : [...value, option];
-      onValueChange(newSelectedValues);
-    };
-
-    const handleTogglePopover = () => {
-      setIsPopoverOpen((prev) => !prev);
-    };
-
-    return (
-      <Popover
-        open={isPopoverOpen}
-        onOpenChange={setIsPopoverOpen}
-        modal={modalPopover}
-      >
-        <PopoverTrigger asChild>
-          <Button
-            ref={ref}
-            {...props}
-            onClick={handleTogglePopover}
-            className={cn(
-              "flex w-full p-1 pl-4 rounded-md border border-border min-h-10 h-auto items-center justify-between bg-inherit hover:bg-inherit",
-              className
-            )}
-          >
-            <div className="flex items-center justify-between w-full mx-auto">
-              <div className="flex items-center">
-                <span className="text-sm text-muted-foreground">
-                  {placeholder}
-                </span>
-              </div>
-              <ChevronDownIcon className="h-4 cursor-pointer text-muted-foreground mx-2" />
-            </div>
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          className="w-[200px] p-0 border-0"
-          align="end"
-          sideOffset={0}
-          side="bottom"
-          onEscapeKeyDown={() => setIsPopoverOpen(false)}
-        >
-          <Command className="flex w-full flex-col rounded-md bg-popover border-border border text-popover-foreground px-2">
-            <CommandPrimitive.Input
-              placeholder="search"
-              className="bg-inherit focus-visible:outline-none px-2 py-2"
-            />
-            <hr className="-mx-2 border-border" />
-            <CommandPrimitive.List className="w-full overflow-y-auto overflow-x-hidden py-2">
-              <CommandEmpty>No results found.</CommandEmpty>
-              <CommandPrimitive.Group>
-                {options.map((option) => {
-                  const isSelected = value.includes(option);
-                  return (
-                    <CommandItem
-                      key={option}
-                      onSelect={() => toggleOption(option)}
-                      className="cursor-pointer flex gap-1"
-                    >
-                      <CheckIcon
-                        size={15}
-                        className={cn(
-                          "flex-none transition-transform duration-75",
-                          isSelected ? "scale-100" : "scale-0"
-                        )}
-                      />
-                      <div className="flex items-center justify-between w-full">
-                        <span>{option}</span>
-                      </div>
-                    </CommandItem>
-                  );
-                })}
-              </CommandPrimitive.Group>
-            </CommandPrimitive.List>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    );
-  }
-);
