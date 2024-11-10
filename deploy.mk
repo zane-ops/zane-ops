61a5f872717fc30f4214bc2d4f1d827dfca75733
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-10 03:17:38 +0100

    🐛 fix typo in deploy makefile (again) (#281)

--- deploy.mk
+++ deploy.mk
@@ -66,22 +66,18 @@ deploy: ### Install and deploy zaneops
 	@echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
 	@echo "    🚀   DEPLOYMENT OF ZANEOPS   🚀"
 	@echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
-	@read -p "Do you want to be the server through HTTP (recommended if you use a reverse tunnel like cloudflare tunnel, or deploying locally) ? (Y/N): " use_http && \
-	if [[ $${use_http} == [yY] || $${use_http} == [yY][eE][sS] ]]; then \
-		set -a; . ./.env; set +a && docker stack deploy --with-registry-auth --compose-file docker-stack.prod.yaml --compose-file docker-stack.prod-http.yaml zane; \
-	else \
-		set -a; . ./.env; set +a && docker stack deploy --with-registry-auth --compose-file docker-stack.prod.yaml zane; \
-	fi
+	@echo "Deploying zaneops...🔄"
+	@set -a; . ./.env; set +a && docker stack deploy --with-registry-auth --compose-file docker-stack.prod.yaml --compose-file docker-stack.prod-http.yaml zane;
 	@. ./attach-proxy-networks.sh
 	@docker service ls --filter "label=zane-managed=true" --filter "label=status=active" -q | xargs -P 0 -I {} docker service scale --detach {}=1
 	@echo "🏁 Deploy done, Please give this is a little minutes before accessing your website 🏁"
 	@echo "You can monitor the services deployed by running \`docker service ls --filter label=\"zane.stack=true\"\`"
-	@echo "Wait for all services (except for `zane_temporal-admin-tools`) to show up as \`replicated   1/1\` to attest that everything started succesfully"
+	@echo "Wait for all services (except for \`zane_temporal-admin-tools\`) to show up as \`replicated   1/1\` to attest that everything started succesfully"
 
 create-user: ### Create the first user to login in into the dashboard
 	@docker exec -it $$(docker ps -qf "name=zane_api") /bin/bash -c "source /venv/bin/activate && python manage.py createsuperuser"
 
-remove: ### Take down zaneops and scale down all services created in zaneops
+stop: ### Take down zaneops and scale down all services created in zaneops
 	@echo "Taking down zaneops..."
 	docker stack rm zane
 	@echo "Scaling down services created in zaneops..., use `make deploy` to restart them"
