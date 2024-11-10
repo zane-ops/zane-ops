9ba55a400c1346e020292f119efaa934b2441d80
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-10 11:05:05 +0100

    🚧 some changes

--- frontend/src/routes/_dashboard/project_/$project_slug_.services.docker.$service_slug.deployments.$deployment_hash/index.tsx
+++ frontend/src/routes/_dashboard/project_/$project_slug_.services.docker.$service_slug.deployments.$deployment_hash/index.tsx
@@ -1,23 +1,30 @@
 import * as Form from "@radix-ui/react-form";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { SearchIcon, XIcon } from "lucide-react";
+import { type VariantProps, cva } from "class-variance-authority";
+import { CheckIcon, ChevronDownIcon, SearchIcon, XIcon } from "lucide-react";
 import * as React from "react";
 import type { DateRange } from "react-day-picker";
 import { DateRangeWithShortcuts } from "~/components/date-range-with-shortcuts";
 import { withAuthRedirect } from "~/components/helper/auth-redirect";
 import { Loader } from "~/components/loader";
 import { Button } from "~/components/ui/button";
-import { Checkbox } from "~/components/ui/checkbox";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from "~/components/ui/select";
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from "~/components/ui/command";
+import { Input } from "~/components/ui/input";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "~/components/ui/popover";
+
 import {
   Tooltip,
   TooltipContent,
@@ -78,115 +85,11 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
 
   return (
     <div className="grid grid-cols-12 gap-4 mt-8">
-      <aside className="col-span-3 h-full hidden">
-        <Form.Root
-          action=""
-          onChange={(e) => {
-            console.log({
-              data: new FormData(e.currentTarget)
-            });
-          }}
-          className="flex gap-3 flex-col sticky top-0"
-        >
-          <div className="flex w-full items-center gap-2">
-            <DateRangeWithShortcuts
-              date={date}
-              setDate={(newDateRange) =>
-                navigate({
-                  search: {
-                    ...filters,
-                    time_before: newDateRange?.to,
-                    time_after: newDateRange?.from
-                  },
-                  replace: true
-                })
-              }
-              className={cn("w-full")}
-            />
-            <Button
-              variant="outline"
-              onClick={() =>
-                navigate({
-                  search: {
-                    ...filters,
-                    time_before: undefined,
-                    time_after: undefined
-                  },
-                  replace: true
-                })
-              }
-              className="py-2.5 h-auto"
-            >
-              <XIcon size={15} className="flex-none" />
-            </Button>
-          </div>
-
-          {/* <div className="flex flex-col gap-1.5">
-            <Label htmlFor="logLevel">Log level</Label>
-            <Select
-              name="level"
-              // value={changedVolumeMode}
-              // onValueChange={(mode) => setChangedVolumeMode(mode as VolumeMode)}
-            >
-              <SelectTrigger id="logLevel">
-                <SelectValue placeholder="Select a log level" />
-              </SelectTrigger>
-              <SelectContent>
-                {LOG_LEVELS.map((level) => (
-                  <SelectItem value={level} key={level} className="capitalize">
-                    {level}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div> */}
-
-          <fieldset className="flex flex-col gap-1.5">
-            <legend>Levels</legend>
-            {LOG_LEVELS.map((level) => (
-              <Form.Field
-                name="source"
-                key={level}
-                className="inline-flex gap-2 items-center"
-              >
-                <Form.Control asChild>
-                  <Checkbox value={level} />
-                </Form.Control>
-
-                <Form.Label className="text-gray-400 inline-flex gap-1 items-center capitalize">
-                  {level}
-                </Form.Label>
-              </Form.Field>
-            ))}
-          </fieldset>
-
-          <fieldset className="flex flex-col gap-1.5">
-            <legend>Sources</legend>
-            {LOG_SOURCES.map((source) => (
-              <Form.Field
-                name="source"
-                key={source}
-                className="inline-flex gap-2 items-center"
-              >
-                <Form.Control asChild>
-                  <Checkbox value={source} />
-                </Form.Control>
-
-                <Form.Label className="text-gray-400 inline-flex gap-1 items-center capitalize">
-                  {source}
-                </Form.Label>
-              </Form.Field>
-            ))}
-          </fieldset>
-        </Form.Root>
-      </aside>
-
       <div className="col-span-12 flex flex-col h-[65vh] gap-2">
         <form
-          action={() => {}}
-          onChange={(e) => {
+          action={(formData) => {
             console.log({
-              change: new FormData(e.currentTarget)
+              data: formData
             });
           }}
           className="rounded-t-sm w-full flex gap-2"
@@ -228,13 +131,13 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
                       <XIcon size={15} className="flex-none" />
                     </Button>
                   </TooltipTrigger>
-                  <TooltipContent>Clear date</TooltipContent>
+                  <TooltipContent>Clear date filter</TooltipContent>
                 </Tooltip>
               </TooltipProvider>
             )}
           </div>
 
-          <div className="flex w-full items-center relative">
+          <div className="flex w-full items-center relative flex-grow">
             <SearchIcon className="absolute left-4 text-grey" />
             <Input
               className="px-14 w-full text-sm  bg-muted/40 dark:bg-card/30"
@@ -248,6 +151,69 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
                 });
               }}
             />
+          </div>
+
+          <div className="flex-shrink-0 flex items-center gap-1.5">
+            {/* <Label htmlFor="logLevel" className="sr-only">
+              Log levels
+            </Label>
+            <Select
+              name="level"
+              // value={changedVolumeMode}
+              // onValueChange={(mode) => setChangedVolumeMode(mode as VolumeMode)}
+            >
+              <SelectTrigger id="logLevel" className="flex-shrink">
+                <SelectValue placeholder="Log levels" />
+              </SelectTrigger>
+              <SelectContent>
+                {LOG_LEVELS.map((level) => (
+                  <SelectItem value={level} key={level} className="capitalize">
+                    {level}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Label htmlFor="logSource" className="sr-only">
+              Log sources
+            </Label> */}
+
+            <SimpleMultiSelect
+              value={filters.level}
+              options={LOG_LEVELS as Writeable<typeof LOG_LEVELS>}
+              onValueChange={(newVal) => {
+                //...
+              }}
+              placeholder="log levels"
+            />
+
+            <SimpleMultiSelect
+              value={filters.source}
+              options={LOG_SOURCES as Writeable<typeof LOG_SOURCES>}
+              onValueChange={(newVal) => {
+                //...
+              }}
+              placeholder="log sources"
+            />
+            {/* <Select
+              name="source"
+              // value={changedVolumeMode}
+              // onValueChange={(mode) => setChangedVolumeMode(mode as VolumeMode)}
+            >
+              <SelectTrigger id="logSource">
+                <SelectValue placeholder="Log sources" />
+              </SelectTrigger>
+              <SelectContent>
+                {LOG_SOURCES.map((source) => (
+                  <SelectItem
+                    value={source}
+                    key={source}
+                    className="capitalize"
+                  >
+                    {source}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select> */}
           </div>
         </form>
         <hr className="border-border" />
@@ -269,3 +235,174 @@ export function DeploymentLogsDetailPage(): React.JSX.Element {
     </div>
   );
 }
+
+/**
+ * Variants for the multi-select component to handle different styles.
+ * Uses class-variance-authority (cva) to define different styles based on "variant" prop.
+ */
+const multiSelectVariants = cva(
+  "m-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-foreground/10 text-foreground bg-card hover:bg-card/80",
+        secondary:
+          "border-foreground/10 bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        inverted: "inverted"
+      }
+    },
+    defaultVariants: {
+      variant: "default"
+    }
+  }
+);
+
+interface MultiSelectProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof multiSelectVariants> {
+  /**
+   * An array of option objects to be displayed in the multi-select component.
+   * Each option object has a label, value, and an optional icon.
+   */
+  options: string[];
+
+  /**
+   * Callback function triggered when the selected values change.
+   * Receives an array of the new selected values.
+   */
+  onValueChange: (value: string[]) => void;
+
+  /**
+   * Placeholder text to be displayed when no values are selected.
+   * Optional, defaults to "Select options".
+   */
+  placeholder?: string;
+
+  /**
+   * Animation duration in seconds for the visual effects (e.g., bouncing badges).
+   * Optional, defaults to 0 (no animation).
+   */
+  animation?: number;
+
+  /**
+   * Maximum number of items to display. Extra selected items will be summarized.
+   * Optional, defaults to 3.
+   */
+  maxCount?: number;
+
+  /**
+   * The modality of the popover. When set to true, interaction with outside elements
+   * will be disabled and only popover content will be visible to screen readers.
+   * Optional, defaults to false.
+   */
+  modalPopover?: boolean;
+
+  /**
+   * If true, renders the multi-select component as a child of another component.
+   * Optional, defaults to false.
+   */
+  asChild?: boolean;
+
+  /**
+   * Additional class names to apply custom styles to the multi-select component.
+   * Optional, can be used to add custom styles.
+   */
+  className?: string;
+  value: string[];
+}
+const SimpleMultiSelect = React.forwardRef<HTMLButtonElement, MultiSelectProps>(
+  (
+    {
+      options,
+      onValueChange,
+      variant,
+      value = [],
+      placeholder = "Select options",
+      animation = 0,
+      maxCount = 3,
+      modalPopover = false,
+      asChild = false,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
+
+    const toggleOption = (option: string) => {
+      const newSelectedValues = value.includes(option)
+        ? value.filter((v) => v !== option)
+        : [...value, option];
+      onValueChange(newSelectedValues);
+    };
+
+    const handleTogglePopover = () => {
+      setIsPopoverOpen((prev) => !prev);
+    };
+
+    return (
+      <Popover
+        open={isPopoverOpen}
+        onOpenChange={setIsPopoverOpen}
+        modal={modalPopover}
+      >
+        <PopoverTrigger asChild>
+          <Button
+            ref={ref}
+            {...props}
+            onClick={handleTogglePopover}
+            className={cn(
+              "flex w-full p-1 pl-4 rounded-md border border-border min-h-10 h-auto items-center justify-between bg-inherit hover:bg-inherit",
+              className
+            )}
+          >
+            <div className="flex items-center justify-between w-full mx-auto">
+              <div className="flex items-center">
+                <span className="text-sm text-muted-foreground">
+                  {placeholder}
+                </span>
+              </div>
+              <ChevronDownIcon className="h-4 cursor-pointer text-muted-foreground mx-2" />
+            </div>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className="w-[150px] p-0 border-0"
+          align="end"
+          sideOffset={0}
+          side="bottom"
+          onEscapeKeyDown={() => setIsPopoverOpen(false)}
+        >
+          <Command>
+            <CommandInput className="hidden" />
+            <CommandList className="w-full ">
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup>
+                {options.map((option) => {
+                  const isSelected = value.includes(option);
+                  return (
+                    <CommandItem
+                      key={option}
+                      onSelect={() => toggleOption(option)}
+                      className="cursor-pointer flex gap-1"
+                    >
+                      {isSelected && (
+                        <CheckIcon size={15} className="flex-none" />
+                      )}
+                      <div className="flex items-center justify-between w-full">
+                        <span>{option}</span>
+                      </div>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    );
+  }
+);
