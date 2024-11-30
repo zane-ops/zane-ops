84c8ee31be5f4a002d0e8398db06e3111f496fc4
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-30 06:08:18 +0100

    ✅ add tests for networks

--- backend/zane_api/tests/networks.py
+++ backend/zane_api/tests/networks.py
@@ -15,5 +15,14 @@ from rest_framework import status
 
 
 class DockerServiceNetworksTests(AuthAPITestCase):
-    def test_apply_simple_changes(self):
+    async def test_service_added_to_global_network(self):
+        p, service = await self.acreate_and_deploy_redis_docker_service()
+
+        deployment: DockerDeployment = await service.deployments.afirst()
+        service = self.fake_docker_client.get_deployment_service(deployment=deployment)
+        service_networks = {net["Target"]: net["Aliases"] for net in service.networks}
+        self.assertTrue("zane" in service_networks)
+
+    async def test_healthcheck_path_uses_service_id_to_run_healthcheck(self):
+        p, service = await self.acreate_and_deploy_caddy_docker_service()
         self.assertTrue(False)
