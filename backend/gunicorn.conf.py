f549857730f717b91eeb112dbf04e5dec2652bb9
Fred KISSIE <fredkiss3@gmail.com>
2024-12-29 06:16:51 +0100

    ♻️ update API service to use configuration file for running

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Use as much workers as there are CPUs available
+import multiprocessing
+
+workers = multiprocessing.cpu_count()
+
+bind = "0.0.0.0:8080"
+loglevel = "info"
