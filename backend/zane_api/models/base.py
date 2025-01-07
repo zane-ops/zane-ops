255bbd8cf33f1f81086441b700fac59ec3803d60
Fred KISSIE <fredkiss3@gmail.com>
2025-01-07 21:45:38 +0100

    🗃️ add `deploy_token` field in service model

--- backend/zane_api/models/base.py
+++ backend/zane_api/models/base.py
@@ -140,6 +140,7 @@ class BaseService(TimestampedModel):
         max_length=255,
         null=True,
     )
+    deploy_token = models.CharField(max_length=20, null=True, unique=True)
 
     @property
     def host_volumes(self):
