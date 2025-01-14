be70c333a3cb5946ce9d4d1f60e36fc3e381763b
Fred KISSIE <fredkiss3@gmail.com>
2025-01-14 10:45:41 +0100

    🔥 delete old `frontend` folder

--- frontend-old/src/tanstack-router.d.ts
+++
@@ -1,7 +0,0 @@
-import {} from "@tanstack/react-router";
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
