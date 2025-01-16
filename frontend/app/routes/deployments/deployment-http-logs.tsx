d6972435b2c2b86c6883f414626b9abd2dcfd4ec
Fred KISSIE <fredkiss3@gmail.com>
2025-01-17 00:22:57 +0100

    🚧 wip

--- frontend/app/routes/deployments/deployment-http-logs.tsx
+++ frontend/app/routes/deployments/deployment-http-logs.tsx
@@ -1,5 +1,26 @@
+import { queryClient } from "~/root";
 import { type Route } from "./+types/deployment-http-logs";
 
-export default function DeploymentHttpLogsPage({}: Route.ComponentProps) {
-  return <>deployment http logs page</>;
+export async function clientLoader({
+  request,
+  params: {
+    deploymentHash: deployment_hash,
+    projectSlug: project_slug,
+    serviceSlug: service_slug
+  }
+}: Route.ClientLoaderArgs) {
+  //
+  // queryClient
+  return {};
+}
+
+export default function DeploymentHttpLogsPage({
+  loaderData,
+  params: {
+    deploymentHash: deployment_hash,
+    projectSlug: project_slug,
+    serviceSlug: service_slug
+  }
+}: Route.ComponentProps) {
+  return <>{/*...*/}</>;
 }
