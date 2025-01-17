c949afb6fc69e3a137ccb02d214869c7e014626c
Fred KISSIE <fredkiss3@gmail.com>
2025-01-17 07:27:29 +0100

    💄 UI fixes

--- frontend/app/routes/deployments/deployment-http-logs.tsx
+++ frontend/app/routes/deployments/deployment-http-logs.tsx
@@ -1,9 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { ArrowDown, Folder, Settings } from "lucide-react";
+import { ChevronsUpDownIcon, LoaderIcon } from "lucide-react";
 import * as React from "react";
-import { Link, useSearchParams } from "react-router";
+import { useSearchParams } from "react-router";
 import type { Writeable } from "zod";
-import type { StatusBadge } from "~/components/status-badge";
 import {
   Table,
   TableBody,
@@ -20,6 +19,7 @@ import {
 } from "~/components/ui/tooltip";
 import {
   type DeploymentHTTPLogFilters,
+  type HttpLog,
   REQUEST_METHODS,
   deploymentHttpLogSearchSchema,
   deploymentQueries
@@ -101,84 +101,128 @@ export default function DeploymentHttpLogsPage({
   const logs = logsQuery.data.pages.flatMap((item) => item.results);
 
   return (
-    <Table>
-      <TableHeader className="bg-toggle">
-        <TableRow className="border-none">
-          <TableHead>
-            <TooltipProvider>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <button
-                    // onClick={() => handleSort("slug")}
-                    className="flex cursor-pointer items-center gap-2"
-                  >
-                    Date
-                    <ArrowDown size={15} className="flex-none" />
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <div className="capitalize">Status</div>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Duration</TableHead>
-          <TableHead>Host</TableHead>
-          <TableHead>Path</TableHead>
-          <TableHead>IP</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {logs.map((log) => {
-          const logTime = formatLogTime(log.time);
-          return (
-            <TableRow className="border-border cursor-pointer" key={log.id}>
-              <TableCell className="font-medium ">
-                <time
-                  className="text-grey"
-                  dateTime={new Date(log.time).toISOString()}
-                >
-                  <span className="sr-only sm:not-sr-only">
-                    {logTime.dateFormat},&nbsp;
-                  </span>
-                  <span>{logTime.hourFormat}</span>
-                </time>
-              </TableCell>
-              <TableCell>{log.request_method}</TableCell>
-              <TableCell
-                className={cn("", {
-                  "text-blue-600": log.status.toString().startsWith("1"),
-                  "text-green-600": log.status.toString().startsWith("2"),
-                  "text-grey": log.status.toString().startsWith("3"),
-                  "text-yellow-600": log.status.toString().startsWith("4"),
-                  "text-red-600": log.status.toString().startsWith("5")
-                })}
+    <div className="flex flex-col h-[60dvh]">
+      <Table className="relative h-full overflow-y-auto z-99">
+        <TableHeader className="bg-toggle sticky top-0">
+          <TableRow className="border-none">
+            <TableHead>
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <button
+                      // onClick={() => handleSort("slug")}
+                      className="flex cursor-pointer items-center gap-2"
+                    >
+                      Date
+                      <ChevronsUpDownIcon size={15} className="flex-none" />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <div className="capitalize">Status</div>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <button
+                      // onClick={() => handleSort("slug")}
+                      className="flex cursor-pointer items-center gap-2"
+                    >
+                      Duration
+                      <ChevronsUpDownIcon size={15} className="flex-none" />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <div className="capitalize">Status</div>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </TableHead>
+            <TableHead>Host</TableHead>
+            <TableHead>Path</TableHead>
+            <TableHead>IP</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <tr className="h-px" />
+          {logs.map((log) => (
+            <LogTableRow log={log} key={log.id} />
+          ))}
+          <TableRow>
+            <TableCell colSpan={7} className="relative">
+              <div
+                className={cn(
+                  "items-center flex gap-2 text-gray-500 px-2",
+                  "w-full sticky left-0"
+                )}
               >
-                {log.status}
-              </TableCell>
-              <TableCell>
-                {Intl.NumberFormat("en-US").format(
-                  log.request_duration_ns / 1_000_000
-                )}
-                <span className="text-grey">ms</span>
-              </TableCell>
-              <TableCell>{log.request_host}</TableCell>
-              <TableCell>
-                {log.request_path}
-                {log.request_query && (
-                  <span className="text-grey">?{log.request_query}</span>
-                )}
-              </TableCell>
-
-              <TableCell>
-                <span className="text-grey">{log.request_ip}</span>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+                <LoaderIcon size={15} className="animate-spin" />
+                <p>Fetching previous logs...</p>
+              </div>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
+}
+
+function LogTableRow({ log }: { log: HttpLog }) {
+  const logTime = formatLogTime(log.time);
+  const ip = log.request_headers["X-Forwarded-For"] ?? log.request_ip;
+  return (
+    <TableRow className="border-border cursor-pointer" key={log.id}>
+      <TableCell>
+        <time
+          className="text-grey whitespace-nowrap"
+          dateTime={new Date(log.time).toISOString()}
+        >
+          <span className="sr-only sm:not-sr-only">
+            {logTime.dateFormat},&nbsp;
+          </span>
+          <span>{logTime.hourFormat}</span>
+        </time>
+      </TableCell>
+      <TableCell>{log.request_method}</TableCell>
+      <TableCell
+        className={cn("", {
+          "text-blue-600": log.status.toString().startsWith("1"),
+          "text-green-600": log.status.toString().startsWith("2"),
+          "text-grey": log.status.toString().startsWith("3"),
+          "text-yellow-600": log.status.toString().startsWith("4"),
+          "text-red-600": log.status.toString().startsWith("5")
+        })}
+      >
+        {log.status}
+      </TableCell>
+      <TableCell>
+        {Intl.NumberFormat("en-US").format(log.request_duration_ns / 1_000_000)}
+        <span className="text-grey">ms</span>
+      </TableCell>
+      <TableCell>
+        <span className="whitespace-nowrap">{log.request_host}</span>
+      </TableCell>
+      <TableCell>
+        <span className="whitespace-nowrap">
+          {log.request_path}
+          {log.request_query && (
+            <span className="text-grey">?{log.request_query}</span>
+          )}
+        </span>
+      </TableCell>
+
+      <TableCell>
+        <span className="text-grey">{ip}</span>
+      </TableCell>
+    </TableRow>
+  );
+}
+
+function HeaderSection() {
+  return;
 }
