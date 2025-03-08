6f19da3414363c8da399c178aa893c873d23da38
Fred KISSIE <fredkiss3@gmail.com>
2025-03-08 05:24:51 +0100

    🚸 ask user for confirmation when deleting project or service

--- frontend/app/routes/services/archive-service.tsx
+++ frontend/app/routes/services/archive-service.tsx
@@ -2,6 +2,7 @@ import { redirect } from "react-router";
 import { toast } from "sonner";
 import { apiClient } from "~/api/client";
 import { projectQueries, resourceQueries, serviceQueries } from "~/lib/queries";
+import type { ErrorResponseFromAPI } from "~/lib/utils";
 import { queryClient } from "~/root";
 import { getCsrfTokenHeader } from "~/utils";
 import { type Route } from "./+types/archive-service";
@@ -13,8 +14,32 @@ export function clientLoader({ params }: Route.ClientLoaderArgs) {
 }
 
 export async function clientAction({
+  request,
   params: { projectSlug: project_slug, serviceSlug: service_slug }
 }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+
+  console.log({
+    service_slug: formData.get("service_slug")?.toString().trim()
+  });
+  if (
+    formData.get("service_slug")?.toString().trim() !==
+    `${project_slug}/${service_slug}`
+  ) {
+    return {
+      errors: {
+        type: "validation_error",
+        errors: [
+          {
+            attr: "service_slug",
+            code: "invalid",
+            detail: "The slug does not match"
+          }
+        ]
+      } satisfies ErrorResponseFromAPI
+    };
+  }
+
   const { error } = await apiClient.DELETE(
     "/api/projects/{project_slug}/archive-service/docker/{service_slug}/",
     {
@@ -50,7 +75,11 @@ export async function clientAction({
 
   toast.success("Success", {
     closeButton: true,
-    description: `Service ${service_slug} has been succesfully archived.`
+    description: (
+      <span>
+        Service `<strong>{service_slug}</strong>` has been succesfully deleted.
+      </span>
+    )
   });
   throw redirect(`/project/${project_slug}`);
 }
