e325605e69f4aa9dbcbc42f796a428e276900f8a
Fred KISSIE <fredkiss3@gmail.com>
2025-04-27 23:10:14 +0200

    🐛 refactor: initialize correctly

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,8 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 
-asgi_application = get_asgi_application()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+asgi_application = get_asgi_application()
 
 from webshell.routing import websocket_urlpatterns
 
