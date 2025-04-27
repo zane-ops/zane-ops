c0aa3800b6c104311f841e080af0b0591e818c0d
Fred KISSIE <fredkiss3@gmail.com>
2025-04-27 23:09:36 +0200

    🐛 initialize asgi application early before importing consumers to fix app not loaded error

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,13 +14,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 
+asgi_application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
 from webshell.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": asgi_application,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
