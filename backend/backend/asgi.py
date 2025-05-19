fbf483970f0b023a81eeaef1c09cb032ffb06a10
Fred KISSIE <fredkiss3@gmail.com>
2025-05-19 21:48:55 +0200

    🔒️ protect websocket endpoints

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -25,11 +25,10 @@ from webshell.routing import websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": asgi_application,
-        "websocket":
-        # AllowedHostsOriginValidator(
-        #     AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns),
-        # )
-        # ),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(websocket_urlpatterns),
+            )
+        ),
     }
 )
