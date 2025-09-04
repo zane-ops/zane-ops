7f0e1bf7017d6d6d564af2a81fd4bd9c17ee1da6
Fred KISSIE <fredkiss3@gmail.com>
2025-09-04 06:10:40 +0200

    💄 show 6 cards in the dashboard

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -221,7 +221,7 @@ function RecentDeploymentsSection() {
       <div
         className={cn(
           "grid gap-4",
-          "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5"
+          "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6"
         )}
       >
         {recentDeployments.map((dpl) => (
