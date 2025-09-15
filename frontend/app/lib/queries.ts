48e54ecf44c93988eec1ae62427c90810401c085
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2025-09-16 01:27:07 +0300

    Update frontend/app/lib/queries.ts
    Co-authored-by: Fred KISSIE <fredkiss3@gmail.com>

--- frontend/app/lib/queries.ts
+++ frontend/app/lib/queries.ts
@@ -25,9 +25,8 @@ export const userQueries = {
   authedUser: queryOptions({
     queryKey: ["AUTHED_USER"] as const,
     queryFn: ({ signal }) => {
-      return apiClient
-        .GET("/api/auth/me/", { signal })
-        .then((res) => res.data?.user);
+      const { data } = await apiClient.GET("/api/auth/me/", { signal });
+      return data?.user;
     },
     refetchInterval: (query) => {
       if (query.state.data) {
