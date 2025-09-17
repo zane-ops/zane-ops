45318bd15def10f54d189483a44559ae8314a993
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2025-09-17 20:04:23 +0300

    fix tests

--- backend/git_connectors/views/gitlab.py
+++ backend/git_connectors/views/gitlab.py
@@ -213,12 +213,11 @@ class TestGitlabAppAPIView(APIView):
             )
         except GitApp.DoesNotExist:
             raise exceptions.NotFound(f"Gitlab app with id {id} does not exist")
-        except HTTPError as error:
-        	print(
-                error.message,
+        except requests.exceptions.HTTPError as error:
+            print(
                 error.response.status_code,
                 error.response.text,
-             )
+            )
             raise BadRequest(
                 "This GitLab app may not be correctly installed or it has been deleted on GitLab. If you're sure the app is installed in GitLab, edit the app to renew the app secret."
             )
