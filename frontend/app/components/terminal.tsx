a489d4835f9d1c6dee6c672f2e0ffd46a22b5b40
Fred KISSIE <fredkiss3@gmail.com>
2025-09-30 20:09:50 +0200

    💄 Light & Dark mode for the terminal

--- frontend/app/components/terminal.tsx
+++ frontend/app/components/terminal.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Terminal as XTermTerminal } from "xterm";
+import { type ITheme, Terminal as XTermTerminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 import "xterm/css/xterm.css";
+import { useTheme } from "~/components/theme-provider";
 import { cn } from "~/lib/utils";
 
 type TerminalProps = {
@@ -9,6 +10,54 @@ type TerminalProps = {
   shellUser?: string | null;
   baseWebSocketURL: string;
   className?: string;
+};
+
+const shellDarkTheme: ITheme = {
+  foreground: "#f8f8f2",
+  background: "#1e1e1e",
+  cursor: "#f8f8f0",
+  cursorAccent: "#1e1e1e",
+  selectionBackground: "#44475a",
+  black: "#21222c",
+  red: "#ff5555",
+  green: "#50fa7b",
+  yellow: "#f1fa8c",
+  blue: "#bd93f9",
+  magenta: "#ff79c6",
+  cyan: "#8be9fd",
+  white: "#f8f8f2",
+  brightBlack: "#6272a4",
+  brightRed: "#ff6e6e",
+  brightGreen: "#69ff94",
+  brightYellow: "#ffffa5",
+  brightBlue: "#d6acff",
+  brightMagenta: "#ff92df",
+  brightCyan: "#a4ffff",
+  brightWhite: "#ffffff"
+};
+
+const shellLightTheme: ITheme = {
+  foreground: "#2e3436",
+  background: "#ffffff",
+  cursor: "#2e3436",
+  cursorAccent: "#ffffff",
+  selectionBackground: "#c1deff",
+  black: "#2e3436",
+  red: "#cc0000",
+  green: "#4e9a06",
+  yellow: "#c4a000",
+  blue: "#3465a4",
+  magenta: "#75507b",
+  cyan: "#06989a",
+  white: "#d3d7cf",
+  brightBlack: "#555753",
+  brightRed: "#ef2929",
+  brightGreen: "#8ae234",
+  brightYellow: "#fce94f",
+  brightBlue: "#729fcf",
+  brightMagenta: "#ad7fa8",
+  brightCyan: "#34e2e2",
+  brightWhite: "#eeeeec"
 };
 
 export function Terminal({
@@ -21,6 +70,8 @@ export function Terminal({
   const term = React.useRef<XTermTerminal>(null);
   const fitAddon = React.useRef<FitAddon>(new FitAddon());
   const socketRef = React.useRef<WebSocket | null>(null);
+
+  const theme = useTheme().theme;
 
   // Send terminal size to backend over WebSocket
   const sendResize = () => {
@@ -49,7 +100,30 @@ export function Terminal({
   }, []);
 
   React.useEffect(() => {
+    if (!term.current) return;
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    let termTheme: ITheme;
+    if (theme === "SYSTEM") {
+      termTheme = darkQuery.matches ? shellDarkTheme : shellLightTheme;
+    } else {
+      termTheme = theme === "DARK" ? shellDarkTheme : shellLightTheme;
+    }
+
+    term.current.options.theme = termTheme;
+  }, [theme]);
+
+  React.useEffect(() => {
     if (!terminalRef.current) return;
+
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    let termTheme: ITheme;
+    if (theme === "SYSTEM") {
+      termTheme = darkQuery.matches ? shellDarkTheme : shellLightTheme;
+    } else {
+      termTheme = theme === "DARK" ? shellDarkTheme : shellLightTheme;
+    }
 
     // 1. Initialize terminal + fit addon
     term.current = new XTermTerminal({
@@ -58,7 +132,8 @@ export function Terminal({
       rows: 24,
       fontFamily:
         '"Geist-Mono", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-      fontSize: 14
+      fontSize: 14,
+      theme: termTheme
     });
     fitAddon.current = new FitAddon();
     term.current.loadAddon(fitAddon.current);
