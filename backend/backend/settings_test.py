72173a16a87b212d0d849c093a95f2597fb45aed
Fred KISSIE <fredkiss3@gmail.com>
2025-11-22 18:00:06 +0100

    👷optimize test running in CI

---
+++ backend/backend/settings_test.py
@@ -0,0 +1,40 @@
+# settings_test.py
+from .settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "zane",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5434",
+        # Performance optimizations for tests
+        "OPTIONS": {
+            "connect_timeout": 10,
+        },
+        "TEST": {
+            "TEMPLATE": "template0",
+        },
+    }
+}
+
+
+# Disable migrations for faster test database setup
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
+
+# Faster password hashing in tests
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+# Disable logging during tests
+LOGGING_CONFIG = None
