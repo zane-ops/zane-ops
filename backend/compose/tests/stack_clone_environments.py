02e7637dc01c3006d065c2cf76ad8d0eaa27dc81
Fred KISSIE <fredkiss3@gmail.com>
2026-01-29 06:29:34 +0100

    🧪 more tests

--- backend/compose/tests/stack_clone_environments.py
+++ backend/compose/tests/stack_clone_environments.py
@@ -8,13 +8,7 @@ from zane_api.models import Environment, SharedEnvVariable
 from ..models import ComposeStack, ComposeStackChange, ComposeStackEnvOverride
 from .fixtures import (
     DOCKER_COMPOSE_MINIMAL,
-    DOCKER_COMPOSE_SIMPLE_DB,
-    DOCKER_COMPOSE_WITH_X_ENV_IN_URLS,
-    DOCKER_COMPOSE_WITH_PLACEHOLDERS,
     DOCKER_COMPOSE_WEB_SERVICE,
-    DOCKER_COMPOSE_WITH_SHARED_ENV_REFERENCES,
-    DOCKER_COMPOSE_WITHOUT_SHARED_ENV_REFERENCES,
-    DOCKER_COMPOSE_WITH_SHARED_ENV_OUTSIDE_X_ZANE_ENV,
 )
 from typing import cast
 from zane_api.utils import jprint
@@ -22,6 +16,7 @@ from temporal.helpers import ZaneProxyClient
 
 from .stacks import ComposeStackAPITestBase
 from ..dtos import ComposeStackUrlRouteDto
+import docker.errors
 
 
 class CloneEnvironmentWithStackViewTests(ComposeStackAPITestBase):
@@ -64,6 +59,47 @@ class CloneEnvironmentWithStackViewTests(ComposeStackAPITestBase):
         )
         self.assertNotEqual(original_stack.deploy_token, cloned_stack.deploy_token)
 
+    def test_clone_environment_with_urls_replace_urls_with_generated_ones_in_compose_content(
+        self,
+    ):
+        p, original_stack = self.create_and_deploy_compose_stack(
+            content=DOCKER_COMPOSE_WEB_SERVICE
+        )
+
+        response = self.client.post(
+            reverse(
+                "zane_api:projects.environment.clone",
+                kwargs={"slug": p.slug, "env_slug": Environment.PRODUCTION_ENV_NAME},
+            ),
+            data={"name": "staging"},
+        )
+        jprint(response.json())
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        staging_env = p.environments.get(name="staging")
+
+        stacks_in_staging = staging_env.compose_stacks
+        self.assertEqual(1, stacks_in_staging.count())
+
+        cloned_stack = cast(ComposeStack, stacks_in_staging.first())
+
+        self.assertEqual(1, cloned_stack.unapplied_changes.count())
+
+        content_change = cast(
+            ComposeStackChange,
+            cloned_stack.unapplied_changes.filter(
+                field=ComposeStackChange.ChangeField.COMPOSE_CONTENT
+            ).first(),
+        )
+        self.assertIsNotNone(content_change)
+        self.assertNotIn(
+            "hello.127-0-0-1.sslip.io", cast(str, content_change.new_value)
+        )
+
+        # it should create an env override in compose stack file
+        self.assertIn("x-zane-env", cast(str, content_change.new_value))
+        self.assertIn("{{ generate_domain }}", cast(str, content_change.new_value))
+
     def test_clone_environment_with_undeployed_stacks_should_include_unapplied_changes_in_diffing(
         self,
     ):
@@ -100,6 +136,44 @@ class CloneEnvironmentWithStackViewTests(ComposeStackAPITestBase):
     async def test_clone_environment_with_deploy_true_should_deploy_included_stacks(
         self,
     ):
+        p, original_stack = await self.acreate_and_deploy_compose_stack(
+            content=DOCKER_COMPOSE_MINIMAL
+        )
+
+        response = await self.async_client.post(
+            reverse(
+                "zane_api:projects.environment.clone",
+                kwargs={"slug": p.slug, "env_slug": Environment.PRODUCTION_ENV_NAME},
+            ),
+            data={"name": "staging"},
+        )
+        jprint(response.json())
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        staging_env = p.environments.get(name="staging")
+
+        stacks_in_staging = staging_env.compose_stacks
+        self.assertEqual(1, stacks_in_staging.count())
+
+        cloned_stack = cast(ComposeStack, stacks_in_staging.first())
+
+        # user content change has been applied
+        self.assertEqual(DOCKER_COMPOSE_MINIMAL.strip(), cloned_stack.user_content)
+
+        self.assertEqual(1, await cloned_stack.deployments.acount())
+
+        service = None
+        try:
+            service = self.fake_docker_client.services_get(
+                f"{cloned_stack.name}_{cloned_stack.hash_prefix}_redis"
+            )
+        except Exception:
+            pass
+        self.assertIsNotNone(service)
+
+    async def test_clone_environment_without_deploy_true_should_not_deploy_included_stacks(
+        self,
+    ):
         p, _ = await self.acreate_and_deploy_compose_stack(
             content=DOCKER_COMPOSE_MINIMAL
         )
@@ -119,20 +193,11 @@ class CloneEnvironmentWithStackViewTests(ComposeStackAPITestBase):
         stacks_in_staging = staging_env.compose_stacks
         self.assertEqual(1, stacks_in_staging.count())
 
-        stack = cast(ComposeStack, stacks_in_staging.first())
+        cloned_stack = cast(ComposeStack, stacks_in_staging.first())
 
-        self.assertEqual(1, await stack.deployments.acount())
+        self.assertEqual(1, await cloned_stack.deployments.acount())
 
-        service = None
-        try:
-            service = self.fake_docker_client.services_get(
-                f"{stack.name}_{stack.hash_prefix}_redis"
+        with self.assertRaises(docker.errors.NotFound):
+            self.fake_docker_client.services_get(
+                f"{cloned_stack.name}_{cloned_stack.hash_prefix}_redis"
             )
-        except Exception:
-            pass
-        self.assertIsNotNone(service)
-
-    async def test_clone_environment_without_deploy_true_should_not_deploy_included_stacks(
-        self,
-    ):
-        pass
