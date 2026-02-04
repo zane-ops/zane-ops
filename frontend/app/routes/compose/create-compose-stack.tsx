c40bd893e7629437caba8c2882383e769558b4a3
Fred KISSIE <fredkiss3@gmail.com>
2026-02-04 02:20:44 +0100

    ✨ create compose stack page

--- frontend/app/routes/compose/create-compose-stack.tsx
+++ frontend/app/routes/compose/create-compose-stack.tsx
@@ -1,4 +1,7 @@
+import { ArrowRightIcon, FileTextIcon } from "lucide-react";
 import { Link } from "react-router";
+import { DokployLogo } from "~/components/dokploy-logo";
+import { LogoSvg } from "~/components/logo";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,6 +10,13 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator
 } from "~/components/ui/breadcrumb";
+import { Button } from "~/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "~/components/ui/tooltip";
 import { cn } from "~/lib/utils";
 import { metaTitle } from "~/utils";
 import type { Route } from "./+types/create-compose-stack";
@@ -69,6 +79,58 @@ export default function CreateComposeStackPage({
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
+      <div className="flex h-[70vh] grow justify-center items-center">
+        <div className="card  flex  md:w-[50%] lg:w-[30%] w-full flex-col gap-6">
+          <h1 className="text-3xl font-bold">New Compose Stack</h1>
+          <div className="flex flex-col gap-3">
+            <TooltipProvider>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Button
+                    type="button"
+                    aria-disabled="true"
+                    variant="secondary"
+                    asChild
+                    className="flex gap-2.5 items-center font-semibold justify-center p-10"
+                  >
+                    <Link to="#">
+                      <LogoSvg className="flex-none size-8" />
+                      <span>From ZaneOps template</span>
+                      <ArrowRightIcon className="flex-none" />
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent side="left">Coming soon</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <Button
+              asChild
+              variant="secondary"
+              className="flex gap-2.5  font-semibold items-center justify-center p-10"
+            >
+              <Link to="./dokploy" prefetch="intent">
+                <DokployLogo className="flex-none size-8" />
+                <span>From Dokploy template</span>
+                <ArrowRightIcon className="flex-none" />
+              </Link>
+            </Button>
+
+            <Button
+              asChild
+              variant="secondary"
+              className="flex gap-2.5 items-center  font-semibold  justify-center p-10"
+            >
+              <Link to="./file" prefetch="intent">
+                <FileTextIcon className="flex-none" />
+                <span>From compose file contents</span>
+                <ArrowRightIcon className="flex-none" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
