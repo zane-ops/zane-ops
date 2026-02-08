bce5cd218c2773f5e81d21006d571c397c677871
Fred KISSIE <fredkiss3@gmail.com>
2026-02-08 02:48:15 +0100

    ♻️ remove useless `get_queryset`

--- backend/compose/views/stacks.py
+++ backend/compose/views/stacks.py
@@ -260,36 +260,6 @@ class ComposeStackCreateAPIView(CreateAPIView):
     serializer_class = ComposeStackSerializer
     queryset = ComposeStack.objects.all()
 
-    def get_queryset(self) -> QuerySet[ComposeStack]:  # type: ignore
-        project_slug = self.kwargs["project_slug"]
-        env_slug = self.kwargs["env_slug"]
-
-        try:
-            project = Project.objects.get(
-                slug=project_slug.lower(),
-                owner=self.request.user,
-            )
-            environment = Environment.objects.get(
-                name=env_slug.lower(), project=project
-            )
-        except Project.DoesNotExist:
-            raise exceptions.NotFound(
-                detail=f"A project with the slug `{project_slug}` does not exist"
-            )
-        except Environment.DoesNotExist:
-            raise exceptions.NotFound(
-                detail=f"An environment with the name `{env_slug}` does not exist in this project"
-            )
-
-        return (
-            ComposeStack.objects.filter(
-                environment=environment,
-                project=project,
-            )
-            .all()
-            .prefetch_related("changes")
-        )
-
     def get_serializer_context(self):
         project_slug = self.kwargs["project_slug"]
         env_slug = self.kwargs["env_slug"]
