8b9c1fdb96fc39cc8a451bc1c0b036c26bb51a68
Fred KISSIE <fredkiss3@gmail.com>
2026-02-16 04:09:08 +0100

    💄 ui fix

--- frontend/app/components/nav-link.tsx
+++ frontend/app/components/nav-link.tsx
@@ -11,7 +11,7 @@ export function NavLink({ className, ...props }: NavLinkProps) {
         "gap-2 inline-flex items-center justify-center whitespace-nowrap px-3 py-2 text-sm font-medium transition-all",
         "focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
         "disabled:pointer-events-none disabled:opacity-50",
-        "dark:text-foreground text-grey",
+        "text-grey",
         "aria-[current=page]:border-b-2 aria-[current=page]:border-card-foreground aria-[current=page]:text-card-foreground",
         className
       )}
