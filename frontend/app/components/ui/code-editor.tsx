2e4e041cc531ccd5e04a75d713f1408726436b8d
Fred KISSIE <fredkiss3@gmail.com>
2026-02-16 01:28:48 +0100

    💄 make code editor full screen

--- frontend/app/components/ui/code-editor.tsx
+++ frontend/app/components/ui/code-editor.tsx
@@ -1,6 +1,15 @@
 import MonacoEditor, { type EditorProps } from "@monaco-editor/react";
 import { useMediaQuery } from "@uidotdev/usehooks";
+import { Maximize2Icon, Minimize2Icon } from "lucide-react";
+import * as React from "react";
 import { useTheme } from "~/components/theme-provider";
+import { Button } from "~/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "~/components/ui/tooltip";
 import { cn } from "~/lib/utils";
 
 export type CodeEditorProps = {
@@ -31,16 +40,51 @@ export function CodeEditor({
     theme === "SYSTEM" ? (isDark ? "DARK" : "LIGHT") : theme;
   const editorTheme = resolvedTheme === "LIGHT" ? "vs-light" : "vs-dark";
 
+  const [isFullScreen, setIsFullScreen] = React.useState(false);
+
+  const isDev = !import.meta.env.PROD;
+
   return (
     <div
       className={cn(
         "resize-y h-52 min-h-52 overflow-y-auto overflow-x-clip max-w-full",
         "border border-border",
         "ring-offset-background focus-within:ring-2 focus-within:ring-ring/60 focus-within:ring-offset-2 outline-hidden",
+        "group",
         hasError && "border-red-500 focus-within:ring-red-500/50",
+        isFullScreen
+          ? "fixed z-90 inset-0 !w-dvw !h-dvh !max-w-dvw !max-h-dvh"
+          : "relative w-fit max-w-full",
+        isDev && isFullScreen && "top-7",
         containerClassName
       )}
     >
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <Button
+              variant="outline"
+              className="absolute top-4 right-4 z-30"
+              onClick={() => {
+                setIsFullScreen((prev) => !prev);
+              }}
+            >
+              <span className="sr-only">
+                {isFullScreen ? "Exit full screen" : "Enter full screen"}
+              </span>
+              {isFullScreen ? (
+                <Minimize2Icon className="size-4 flex-none" />
+              ) : (
+                <Maximize2Icon className="size-4 flex-none" />
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent className="max-w-64 text-balance">
+            {isFullScreen ? "Exit full screen" : "Enter full screen"}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
       <MonacoEditor
         className={cn("w-full h-full max-w-full", className)}
         language={language}
