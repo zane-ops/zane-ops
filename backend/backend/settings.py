78f0923b7265cf2da2d05c955ecddace687a672a
Fred KISSIE <fredkiss3@gmail.com>
2026-05-13 20:10:56 +0200

    ♻️  set default permisison to guest

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -277,6 +277,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_PERMISSION_CLASSES": [
         "zane_api.permissions.HasWorkspace",
+        "zane_api.permissions.IsWorkspaceGuest",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
