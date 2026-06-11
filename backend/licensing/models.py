e9919897894dddb4f83bf17dd8b58572c52a1c8b
Fred KISSIE <fredkiss3@gmail.com>
2026-06-11 17:31:29 +0200

    🚧 wip

--- backend/licensing/models.py
+++ backend/licensing/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+
 
 # Create your models here.
+class License(models.Model):
+    data = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class InstanceSettings(models.Model):
+    id = ShortUUIDField(  # type: ignore
+        length=32,
+        max_length=255,
+        primary_key=True,
+        prefix="ist_",
+    )
