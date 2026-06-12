bf02f5cde507ec9f1644d87972999b0b77c1e2bc
Fred KISSIE <fredkiss3@gmail.com>
2026-06-12 19:54:39 +0200

    🚧 wip: license install workflow

---
+++ backend/licensing/tests/license_install.py
@@ -0,0 +1,7 @@
+"""
+License install workflow:
+1. Install from a UUID
+2. Download the license data from the remote API (using the UUID)
+3. Validate license data with public key
+4. Save license data in the DB
+"""
