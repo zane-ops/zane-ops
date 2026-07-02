93e13573d45b7330edeb54ce3d86aea0889ee59f
Fred KISSIE <fredkiss3@gmail.com>
2026-07-02 02:33:55 +0200

    ♻️ refactor

--- backend/scripts/run_daphne.sh
+++ backend/scripts/run_daphne.sh
@@ -4,6 +4,6 @@ wait-for-it zane.loki:3100 -t 0 -- wait-for-it zane.temporal:7233 -t 0 --
 source $VIRTUAL_ENV/bin/activate
 python manage.py migrate 
 python manage.py delete_old_cleanup_schedules
-python manage.py create_metrics_cleanup_schedule 
+python manage.py create_app_data_cleanup_schedule 
 python manage.py create_docker_system_prune_schedule
 daphne -u /app/daphne/daphne.sock backend.asgi:application
