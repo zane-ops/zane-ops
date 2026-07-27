cdf089b9bda42a7538d84120c2ca64568780f046
Fred KISSIE <fredkiss3@gmail.com>
2026-07-27 03:12:14 +0100

    ⏪ Default role min is viewer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -293,7 +293,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_PERMISSION_CLASSES": [
         "zane_api.permissions.HasWorkspace",
-        "zane_api.permissions.IsWorkspaceMember",
+        "zane_api.permissions.IsWorkspaceViewer",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
