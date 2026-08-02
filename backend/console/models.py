76448db12dbeef863a9a9260567602562fff59c0
Fred KISSIE <fredkiss3@gmail.com>
2026-08-02 18:02:02 +0200

    🔥 remove proxy config models & test codes for now

--- backend/console/models.py
+++ backend/console/models.py
@@ -8,32 +8,6 @@ from django.core.validators import MinValueValidator
 from zane_api.utils import convert_value_to_bytes
 
 
-# class CustomProxyConfig(TimestampedModel):
-#     """
-#     A custom Caddyfile snippet added by the instance owner to the ZaneOps proxy,
-#     it is adapted to JSON & installed in the proxy as a single route
-#     identified by `{CADDY_ID_PREFIX}{id}`.
-#     """
-
-#     ID_PREFIX = "prx_cfg_"
-#     CADDY_ID_PREFIX = "zane-custom-config-"
-
-#     id = ShortUUIDField(length=11, max_length=255, primary_key=True, prefix=ID_PREFIX)  # type: ignore
-#     slug = models.SlugField(max_length=255, unique=True)
-#     contents = models.TextField()
-#     enabled = models.BooleanField(default=True)
-
-#     @property
-#     def caddy_route_id(self) -> str:
-#         return f"{self.CADDY_ID_PREFIX}{self.id}"
-
-#     class Meta:  # type: ignore
-#         ordering = ("-updated_at",)
-
-#     def __str__(self):
-#         return f"CustomProxyConfig({self.slug})"
-
-
 class PasswordResetToken(TimestampedModel):
     value = models.CharField(
         max_length=255,
--- backend/console/tests/__init__.py
+++ backend/console/tests/__init__.py
@@ -1,4 +1,3 @@
 from .users import *
 from .workspaces import *
 from .proxy_logs import *
-from .proxy_configs import *
