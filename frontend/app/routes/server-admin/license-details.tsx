811d2b72720f4a4c6e24847b361e4d4c98f49ed0
Fred KISSIE <fredkiss3@gmail.com>
2026-08-10 21:03:30 +0200

    💄 license card UI 2

--- frontend/app/routes/server-admin/license-details.tsx
+++ frontend/app/routes/server-admin/license-details.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import {
   CheckIcon,
+  InfoIcon,
   KeyRoundIcon,
   RefreshCwIcon,
   ScaleIcon,
@@ -8,15 +9,22 @@ import {
 } from "lucide-react";
 import type * as React from "react";
 import type { License } from "~/api/types";
+import { CopyButton } from "~/components/copy-button";
 import { StatusBadge } from "~/components/status-badge";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardFooter, CardTitle } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "~/components/ui/tooltip";
 import { BUILD_EDITION } from "~/lib/constants";
 import { syncLicenseStore } from "~/lib/license-store";
 import { licenseQueries } from "~/lib/queries";
 import { getQueryClient } from "~/lib/query-client";
-import { formattedTime, metaTitle, notFound } from "~/lib/utils";
+import { cn, formattedTime, metaTitle, notFound } from "~/lib/utils";
 import type { Route } from "./+types/license-details";
 
 export function meta() {
@@ -81,42 +89,79 @@ export type LicenseCardProps = {
 };
 
 export function LicenseCard({ license }: LicenseCardProps) {
+  const LICENSE_TIER_DESCRIPTION_MAP = {
+    free: "This license lets you run a single workspace with up to 3 users on this instance.",
+    starter:
+      "This license allows you to run as many workspaces and invite as many users as you need on this instance."
+  } satisfies Record<License["tier"], string>;
   return (
-    <Card className="px-6 py-6 bg-toggle flex flex-col gap-1">
-      <div className=" rounded-md">
-        <div className="font-normal flex gap-3 items-center mb-4">
-          <ScaleIcon className="size-9 flex-none" />
+    <Card className="p-1 border-gray-600 flex flex-col gap-1  w-full border-dashed">
+      <div className="bg-gray-600/10 p-4 rounded-t-md">
+        <div className="font-normal flex gap-3 items-center mb-5">
+          <ScaleIcon className="size-7 flex-none" />
           <div className="flex flex-col gap-0">
             <span className="text-xs text-grey">
               Your host is licensed under the
             </span>
             <div className="flex items-center gap-2">
-              <h3 className="capitalize font-medium text-2xl ">
-                ZaneOps {license.tier} License{" "}
+              <h3 className="capitalize font-medium text-xl">
+                ZaneOps{" "}
+                <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger asChild>
+                      <span className="cursor-help inline-flex items-center gap-1 underline decoration-wavy decoration-1">
+                        {license.tier}
+                      </span>
+                    </TooltipTrigger>
+                    <TooltipContent className="max-w-64">
+                      <p className="text-sm font-normal">
+                        {LICENSE_TIER_DESCRIPTION_MAP[license.tier]}
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>{" "}
+                License{" "}
               </h3>
               {license.is_valid ? (
                 <StatusBadge color="green" pingState="hidden" className="gap-1">
                   Valid
-                  <CheckIcon className="flex-none size-4 text-teal-600 dark:text-teal-400" />
+                  <CheckIcon className="flex-none size-4 text-green-600" />
                 </StatusBadge>
               ) : (
                 <StatusBadge color="red" pingState="hidden" className="gap-1">
                   Invalid
-                  <XIcon className="flex-none size-4 text-red-400" />
+                  <XIcon className="flex-none size-4 text-red-600" />
                 </StatusBadge>
               )}
             </div>
           </div>
         </div>
 
-        <CardContent className="px-0 py-4">
+        <CardContent className="px-0 pb-4 text-sm">
           <dl className="flex flex-col gap-2">
             <div>
-              <dt className="font-medium">Instance Fingerprint:</dt>
-              <dd className="text-grey">{license.instance_fingerprint}</dd>
+              <dt>Instance Fingerprint:</dt>
+              <dd className="text-grey w-full flex items-center gap-2">
+                <p className="truncate">{license.instance_fingerprint}</p>
+                <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger asChild>
+                      <CopyButton
+                        value={license.instance_fingerprint}
+                        label="Copy instance fingerprint"
+                        size="icon"
+                        className="hover:bg-transparent !opacity-100 size-4"
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent className="capitalize">
+                      Copy instance fingerprint
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </dd>
             </div>
             <div>
-              <dt className="font-medium">Expires At:</dt>
+              <dt>Expires At:</dt>
               <dd className="text-grey">
                 <time dateTime={new Date(license.expires_at).toISOString()}>
                   {formattedTime(license.expires_at)}
@@ -124,13 +169,30 @@ export function LicenseCard({ license }: LicenseCardProps) {
               </dd>
             </div>
             <div>
-              <dt className="font-medium">License Key:</dt>
-              <dd className="text-grey">{license.uuid}</dd>
+              <dt>License Key:</dt>
+              <dd className="text-grey flex items-center gap-2">
+                <p className="truncate uppercase">{license.uuid}</p>
+                <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger asChild>
+                      <CopyButton
+                        value={license.uuid}
+                        label="Copy license key"
+                        size="icon"
+                        className="hover:bg-transparent !opacity-100 size-4"
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent className="capitalize">
+                      Copy license key
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </dd>
             </div>
           </dl>
         </CardContent>
       </div>
-      <CardFooter className="pt-6 pb-0 px-6 flex border-t border-border -mx-6">
+      <CardFooter className="py-2 px-3 bg-gray-600/10 flex rounded-b-md">
         <Button variant="outline">Recheck license</Button>
       </CardFooter>
     </Card>
