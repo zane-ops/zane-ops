6ef388e18c211c904b65afd0a3535a8567ea5f98
Fred KISSIE <fredkiss3@gmail.com>
2026-08-10 17:02:20 +0200

    💄 Design 1 of license card

--- frontend/app/routes/server-admin/license-details.tsx
+++ frontend/app/routes/server-admin/license-details.tsx
@@ -1,15 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
-import { KeyRoundIcon, RefreshCwIcon } from "lucide-react";
+import {
+  CheckIcon,
+  KeyRoundIcon,
+  RefreshCwIcon,
+  ScaleIcon,
+  XIcon
+} from "lucide-react";
 import type * as React from "react";
 import type { License } from "~/api/types";
+import { StatusBadge } from "~/components/status-badge";
 import { Button } from "~/components/ui/button";
-import { Card } from "~/components/ui/card";
+import { Card, CardContent, CardFooter, CardTitle } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
 import { BUILD_EDITION } from "~/lib/constants";
 import { syncLicenseStore } from "~/lib/license-store";
 import { licenseQueries } from "~/lib/queries";
 import { getQueryClient } from "~/lib/query-client";
-import { metaTitle, notFound } from "~/lib/utils";
+import { formattedTime, metaTitle, notFound } from "~/lib/utils";
 import type { Route } from "./+types/license-details";
 
 export function meta() {
@@ -49,7 +56,7 @@ export default function LicenseDetailsPage({
         )}
       </div>
       <Separator />
-      <h3 className="text-grey">Manage your ZaneOps License</h3>
+      {/* <h3 className="text-grey">Manage your ZaneOps License</h3> */}
       {license ? (
         <LicenseCard license={license} />
       ) : (
@@ -74,7 +81,60 @@ export type LicenseCardProps = {
 };
 
 export function LicenseCard({ license }: LicenseCardProps) {
-  return <Card className="p-4"></Card>;
+  return (
+    <Card className="px-6 py-6 bg-toggle flex flex-col gap-1">
+      <div className=" rounded-md">
+        <div className="font-normal flex gap-3 items-center mb-4">
+          <ScaleIcon className="size-9 flex-none" />
+          <div className="flex flex-col gap-0">
+            <span className="text-xs text-grey">
+              Your host is licensed under the
+            </span>
+            <div className="flex items-center gap-2">
+              <h3 className="capitalize font-medium text-2xl ">
+                ZaneOps {license.tier} License{" "}
+              </h3>
+              {license.is_valid ? (
+                <StatusBadge color="green" pingState="hidden" className="gap-1">
+                  Valid
+                  <CheckIcon className="flex-none size-4 text-teal-600 dark:text-teal-400" />
+                </StatusBadge>
+              ) : (
+                <StatusBadge color="red" pingState="hidden" className="gap-1">
+                  Invalid
+                  <XIcon className="flex-none size-4 text-red-400" />
+                </StatusBadge>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <CardContent className="px-0 py-4">
+          <dl className="flex flex-col gap-2">
+            <div>
+              <dt className="font-medium">Instance Fingerprint:</dt>
+              <dd className="text-grey">{license.instance_fingerprint}</dd>
+            </div>
+            <div>
+              <dt className="font-medium">Expires At:</dt>
+              <dd className="text-grey">
+                <time dateTime={new Date(license.expires_at).toISOString()}>
+                  {formattedTime(license.expires_at)}
+                </time>
+              </dd>
+            </div>
+            <div>
+              <dt className="font-medium">License Key:</dt>
+              <dd className="text-grey">{license.uuid}</dd>
+            </div>
+          </dl>
+        </CardContent>
+      </div>
+      <CardFooter className="pt-6 pb-0 px-6 flex border-t border-border -mx-6">
+        <Button variant="outline">Recheck license</Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export type ActivateLicenseDialogProps = {
