eddf12562aa3a05eafe2f2ad2ae2cb2632885168
Fred KISSIE <fredkiss3@gmail.com>
2026-09-10 16:09:51 +0200

    🚧 model

--- backend/swarm/models.py
+++ backend/swarm/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from webshell.models import SSHKey
+
+
+class SwarmNodeType(models.TextChoices):
+    MANAGER = "MANAGER", "Manager"
+    WORKER = "WORKER", "Worker"
+
 
 # Create your models here.
+class ServerNode(models.Model):
+    ID_PREFIX = "tok_"
+    ID_LENGTH = 11
+
+    id = ShortUUIDField(
+        length=ID_LENGTH,
+        max_length=255,
+        primary_key=True,
+        prefix=ID_PREFIX,
+    )  # type: ignore
+
+    swarm_node = models.CharField(choices=SwarmNodeType.choices)
+    swarm_id = models.CharField(unique=True)
+    ip = models.CharField()
+    docker_version = models.CharField()
+    ssh_key = models.ForeignKey(
+        to=SSHKey,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
