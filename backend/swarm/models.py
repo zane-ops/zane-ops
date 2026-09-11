2f831a7f9a0915759c3eec9b0487b42559176f47
Fred KISSIE <fredkiss3@gmail.com>
2026-09-11 19:58:09 +0200

    🗃️ DB

--- backend/swarm/models.py
+++ backend/swarm/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 from webshell.models import SSHKey
-
-
-class SwarmNodeType(models.TextChoices):
-    MANAGER = "MANAGER", "Manager"
-    WORKER = "WORKER", "Worker"
+from zane_api.models.base import TimestampedModel
 
 
 # Create your models here.
-class ServerNode(models.Model):
-    ID_PREFIX = "tok_"
+class ServerNode(TimestampedModel):
+    ID_PREFIX = "node_"
     ID_LENGTH = 11
+
+    class Role(models.TextChoices):
+        MANAGER = "MANAGER", "Manager"
+        WORKER = "WORKER", "Worker"
+
+    class Status(models.TextChoices):
+        PROVISIONING = "PROVISIONING", "Provisioning"
+        READY = "READY", "Ready"
+        DOWN = "DOWN", "Down"
+        DRAINED = "DRAINED", "Drained"
+        FAILED = "FAILED", "Failed"
 
     id = ShortUUIDField(
         length=ID_LENGTH,
@@ -20,12 +27,17 @@ class ServerNode(models.Model):
         prefix=ID_PREFIX,
     )  # type: ignore
 
-    swarm_node = models.CharField(choices=SwarmNodeType.choices)
-    swarm_id = models.CharField(unique=True)
-    ip = models.CharField()
+    hostname = models.CharField(unique=True)  # == swarm node Description.Hostname
+    swarm_node_id = models.CharField(unique=True)
+    role = models.CharField(choices=Role.choices)
+    private_ip = models.GenericIPAddressField()  # overlay / VPC address
+    public_ip = models.GenericIPAddressField(null=True)
+
+    status = models.CharField(choices=Status.choices)
     docker_version = models.CharField()
     ssh_key = models.ForeignKey(
         to=SSHKey,
         on_delete=models.SET_NULL,
         null=True,
     )
+    last_status_update = models.DateTimeField(null=True)
