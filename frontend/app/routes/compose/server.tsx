d057b1d61e4af875e7d29ccf79f6f096bc509bbd
Fred KISSIE <fredkiss3@gmail.com>
2026-09-16 16:58:55 +0200

    💄 server card

--- frontend/app/routes/compose/server.tsx
+++
@@ -1,5 +0,0 @@
-export type ServerProps = {};
-
-export function Server({}: ServerProps) {
-  return <></>;
-}
--- frontend/app/routes/server-admin/server-list.tsx
+++ frontend/app/routes/server-admin/server-list.tsx
@@ -1,11 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
-import { Link, useSearchParams } from "react-router";
+import { Link, href, useSearchParams } from "react-router";
 
-import { PlusIcon } from "lucide-react";
+import {
+  AtSignIcon,
+  CpuIcon,
+  CrownIcon,
+  GlobeIcon,
+  HeartPulseIcon,
+  HourglassIcon,
+  LoaderIcon,
+  LogsIcon,
+  type LucideIcon,
+  MemoryStickIcon,
+  NetworkIcon,
+  PlusIcon,
+  PowerOffIcon,
+  ServerIcon,
+  SquareTerminalIcon,
+  TerminalIcon,
+  TextSearchIcon,
+  TriangleAlertIcon,
+  XIcon
+} from "lucide-react";
 import type { SwarmNode } from "~/api/types";
+import { Code } from "~/components/code";
+import { DockerHubLogo } from "~/components/docker-hub-logo";
 import { Pagination } from "~/components/pagination";
 import { StatusBadge, type StatusBadgeColor } from "~/components/status-badge";
+import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardFooter } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
 import {
   Table,
@@ -15,13 +39,27 @@ import {
   TableHeader,
   TableRow
 } from "~/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "~/components/ui/tooltip";
 import { swarmNodeListFilters, swarmQueries } from "~/lib/queries";
 import { getQueryClient } from "~/lib/query-client";
-import { formatStorageValue, metaTitle } from "~/lib/utils";
+import {
+  capitalizeText,
+  cn,
+  formatStorageValue,
+  metaTitle,
+  pluralize
+} from "~/lib/utils";
 import type { Route } from "./+types/server-list";
 
 export function meta() {
-  return [metaTitle("Servers")] satisfies ReturnType<Route.MetaFunction>;
+  return [
+    metaTitle("ZaneOps Cluster")
+  ] satisfies ReturnType<Route.MetaFunction>;
 }
 
 export async function clientLoader({ request }: Route.ClientLoaderArgs) {
@@ -62,7 +100,7 @@ export default function ServerListPage({ loaderData }: Route.ComponentProps) {
   return (
     <section className="flex flex-col gap-4">
       <div className="flex items-center gap-4">
-        <h2 className="text-2xl">Servers</h2>
+        <h2 className="text-2xl">ZaneOps Cluster</h2>
         <Button asChild variant="secondary" className="flex gap-2">
           <Link to="new" prefetch="intent">
             Add server <PlusIcon size={18} />
@@ -75,7 +113,11 @@ export default function ServerListPage({ loaderData }: Route.ComponentProps) {
         can be deployed to any server in this list.
       </h3>
 
-      {/* <ServerTable nodes={nodes} /> */}
+      <ul>
+        {nodes.map((node) => (
+          <ServerCard key={node.id} {...node} />
+        ))}
+      </ul>
 
       <div className="my-4 block">
         {nodes.length > 0 && data.count > 10 && (
@@ -103,93 +145,273 @@ export default function ServerListPage({ loaderData }: Route.ComponentProps) {
   );
 }
 
-const STATUS_COLORS = {
+export function ServerCard({
+  hostname,
+  cpus,
+  memory_bytes,
+  docker_version,
+  is_app_server,
+  is_build_server,
+  status,
+  role,
+  private_ip,
+  is_initial_install_server
+}: SwarmNode) {
+  const memory =
+    memory_bytes !== null ? formatStorageValue(memory_bytes) : null;
+  return (
+    <Card>
+      <CardContent className="rounded-md bg-toggle p-0">
+        <div className="flex items-center gap-4.5 p-4 ">
+          <div className="flex flex-col items-center p-3 rounded-md gap-3">
+            <ServerIcon className="size-8 text-grey flex-none" />
+            <Badge variant="outline">server</Badge>
+          </div>
+
+          <div className="flex flex-col gap-1.5 items-start">
+            <h3 className="font-medium text-lg">{hostname}</h3>
+
+            <div className="flex items-center gap-1">
+              {is_initial_install_server && (
+                <div className="py-1 text-sm rounded-md bg-link/20 text-link px-2  inline-flex gap-1 items-center">
+                  <CrownIcon className="size-4 flex-none" />
+                  <p>Main server</p>
+                </div>
+              )}
+              <ServerStatusBadge status={status} className="text-sm" />
+            </div>
+
+            <div className="flex items-center gap-2.5 text-sm">
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <span className="flex items-center gap-0.5 group cursor-help">
+                      <GlobeIcon className="size-4 text-grey flex-none" />
+                      <span className="sr-only">private IP:</span>
+                      &nbsp;
+                      <span className="group-hover:underline decoration-wavy decoration-1 ">
+                        {private_ip}
+                      </span>
+                    </span>
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-64">
+                    Private IP
+                  </TooltipContent>
+                </Tooltip>
+
+                <span>&middot;</span>
+
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <span className="flex items-center gap-0.5 group cursor-help">
+                      <NetworkIcon className="size-3.5 flex-none text-grey" />
+                      <span className="sr-only">Swarm Role:</span>
+                      &nbsp;
+                      <span className="group-hover:underline decoration-wavy decoration-1">
+                        {capitalizeText(role)}
+                      </span>
+                    </span>
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-64">
+                    <span className="text-grey dark:text-card-foreground">
+                      Swarm Role:
+                    </span>
+                    &nbsp;
+                    {role === "MANAGER"
+                      ? "Manager · maintains the cluster state and schedules services onto the other servers."
+                      : "Worker · only runs the services assigned to it by the managers."}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
+
+          <div className="flex-grow"></div>
+
+          <div className="self-start flex items-start gap-1 h-full pt-1">
+            <TooltipProvider>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Button size="sm" variant="ghost" asChild>
+                    <Link
+                      to={{
+                        pathname: href("/admin/server-console")
+                      }}
+                    >
+                      <TerminalIcon className="size-4" />
+                      <span className="sr-only">
+                        Access to the server's console
+                      </span>
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Access to the server's console</TooltipContent>
+              </Tooltip>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Button size="sm" variant="ghost" asChild>
+                    <Link
+                      to={{
+                        pathname: href("/admin/server-console")
+                      }}
+                    >
+                      <TextSearchIcon className="size-4" />
+                      <span className="sr-only">See deployment logs</span>
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>See server provisionning logs</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        </div>
+
+        <CardFooter className="border-border border-t flex items-center gap-2 text-sm py-2 px-4">
+          <TooltipProvider>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <span className="flex items-center gap-1">
+                  <CpuIcon className="size-4 flex-none text-grey" />
+                  <span className="sr-only">CPU count:</span>
+                  {cpus !== null ? (
+                    <span>
+                      {cpus} {pluralize("CPU", cpus)}
+                    </span>
+                  ) : (
+                    "-"
+                  )}
+                </span>
+              </TooltipTrigger>
+              <TooltipContent>CPU count</TooltipContent>
+            </Tooltip>
+
+            <span>&middot;</span>
+
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <span className="flex items-center gap-1">
+                  <MemoryStickIcon className="size-4 flex-none text-grey" />
+                  <span className="sr-only">Memory:</span>
+                  {memory ? `${memory.value} ${memory.unit}` : "-"}
+                </span>
+              </TooltipTrigger>
+              <TooltipContent>Memory</TooltipContent>
+            </Tooltip>
+
+            <span>&middot;</span>
+
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <span className="flex items-center gap-1">
+                  <DockerHubLogo className="size-4 flex-none text-grey" />
+                  <span className="sr-only">Docker version:</span>v
+                  {docker_version}
+                </span>
+              </TooltipTrigger>
+              <TooltipContent>Docker version</TooltipContent>
+            </Tooltip>
+
+            <span className="w-px bg-muted h-3" />
+
+            <div className="flex items-center gap-1">
+              {is_app_server && (
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Code className="inline-flex items-center gap-1 cursor-help">
+                      app server
+                    </Code>
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-64">
+                    <span className="dark:text-card-foreground text-grey">
+                      App server:
+                    </span>{" "}
+                    your services can be deployed and run on this server.
+                  </TooltipContent>
+                </Tooltip>
+              )}
+              {is_build_server && (
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Code className="inline-flex items-center gap-1 cursor-help">
+                      build server
+                    </Code>
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-64">
+                    <span className="dark:text-card-foreground text-grey">
+                      Build server:
+                    </span>{" "}
+                    docker images for git services are built on this server.
+                  </TooltipContent>
+                </Tooltip>
+              )}
+            </div>
+          </TooltipProvider>
+        </CardFooter>
+      </CardContent>
+    </Card>
+  );
+}
+
+const SERVER_STATUS_COLOR_MAP = {
   READY: "green",
   PROVISIONING: "blue",
   DOWN: "red",
   FAILED: "red",
   DRAINED: "gray"
-} satisfies Record<SwarmNode["status"], StatusBadgeColor>;
+} as const satisfies Record<SwarmNode["status"], StatusBadgeColor>;
 
-type ServerTableProps = {
-  nodes: SwarmNode[];
+type ServerStatusBadgeProps = {
+  status: SwarmNode["status"];
+  className?: string;
+  variant?: "outline" | "ghost";
 };
 
-function ServerTable({ nodes }: ServerTableProps) {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="sticky top-0 z-20">Hostname</TableHead>
-          <TableHead className="sticky top-0 z-20">Role</TableHead>
-          <TableHead className="sticky top-0 z-20">Status</TableHead>
-          <TableHead className="sticky top-0 z-20 whitespace-nowrap">
-            Private IP
-          </TableHead>
-          <TableHead className="sticky top-0 z-20">Resources</TableHead>
-          <TableHead className="sticky top-0 z-20">Docker</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {nodes.length === 0 ? (
-          <TableRow className="px-2">
-            <TableCell colSpan={6} className="p-2 text-muted-foreground italic">
-              -- No server found --
-            </TableCell>
-          </TableRow>
-        ) : (
-          nodes.map((node) => {
-            const memory =
-              node.memory_bytes !== null
-                ? formatStorageValue(node.memory_bytes)
-                : null;
+export function ServerStatusBadge({
+  status,
+  className,
+  variant = "ghost"
+}: ServerStatusBadgeProps) {
+  const color = SERVER_STATUS_COLOR_MAP[status];
 
-            return (
-              <TableRow className="px-2" key={node.id}>
-                <TableCell className="p-2">
-                  <div className="flex items-center gap-2">
-                    <span>{node.hostname}</span>
-                    {node.is_initial_install_server && (
-                      <StatusBadge
-                        color="gray"
-                        pingState="hidden"
-                        className="text-xs py-0.5 px-1.5"
-                      >
-                        initial
-                      </StatusBadge>
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell className="p-2 capitalize">
-                  {node.role.toLowerCase()}
-                </TableCell>
-                <TableCell className="p-2">
-                  <StatusBadge
-                    color={STATUS_COLORS[node.status]}
-                    pingState={
-                      node.status === "PROVISIONING" ? "animated" : "static"
-                    }
-                    className="text-xs py-0.5 px-1.5 capitalize"
-                  >
-                    {node.status.toLowerCase()}
-                  </StatusBadge>
-                </TableCell>
-                <TableCell className="p-2">
-                  <code className="text-sm">{node.private_ip}</code>
-                </TableCell>
-                <TableCell className="p-2 text-grey whitespace-nowrap">
-                  {node.cpus !== null ? `${node.cpus} CPU` : "-"}
-                  &nbsp;/&nbsp;
-                  {memory ? `${memory.value} ${memory.unit}` : "-"}
-                </TableCell>
-                <TableCell className="p-2 text-grey">
-                  {node.docker_version}
-                </TableCell>
-              </TableRow>
-            );
-          })
+  const icons = {
+    READY: HeartPulseIcon,
+    PROVISIONING: HourglassIcon,
+    DOWN: PowerOffIcon,
+    FAILED: XIcon,
+    DRAINED: TriangleAlertIcon
+  } as const satisfies Record<typeof status, LucideIcon>;
+
+  const Icon = icons[status];
+
+  const isLoading = status === "PROVISIONING";
+  const isActive = status === "READY";
+
+  return (
+    <div
+      className={cn(
+        " rounded-md bg-link/20 text-link px-2 py-1  inline-flex gap-1 items-center",
+        {
+          "bg-emerald-400/20 dark:bg-emerald-600/20 text-green-600  dark:text-emerald-400":
+            color === "green",
+          "bg-red-600/10 text-red-600 dark:text-red-400": color === "red",
+          "bg-gray-600/20 dark:bg-gray-600/60 text-gray": color === "gray",
+          "bg-link/20 text-link": color === "blue"
+        },
+        variant === "outline" && "!bg-transparent",
+        className
+      )}
+    >
+      <div className="relative">
+        {isActive && (
+          <Icon
+            size={15}
+            className="flex-none animate-ping absolute h-full w-full"
+          />
         )}
-      </TableBody>
-    </Table>
+        <Icon size={15} className="flex-none" />
+      </div>
+      <p>{capitalizeText(status.replaceAll("_", " "))}</p>
+      {isLoading && <LoaderIcon className="animate-spin flex-none" size={15} />}
+    </div>
   );
 }
