0d6c7474985d3b8600960fa104fa77c18bcb84bd
Fred KISSIE <fredkiss3@gmail.com>
2026-09-25 17:23:05 +0200

    🔒️ Add required scopes permissions by default to prevent invalid scoped tokens

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -294,6 +294,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "zane_api.permissions.HasWorkspace",
         "zane_api.permissions.IsWorkspaceViewer",
+        "zane_api.permissions.HasRequiredScopes",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
