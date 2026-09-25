f5c29466a0e73279c52cdabd0eabcbe9b44658e8
Fred KISSIE <fredkiss3@gmail.com>
2026-09-25 22:48:29 +0200

    💄 Update settings layouts

--- frontend/app/routes/layouts/workspace-settings-layout.tsx
+++ frontend/app/routes/layouts/workspace-settings-layout.tsx
@@ -69,7 +69,7 @@ export default function SettingsLayoutPage({}: Route.ComponentProps) {
 
   return (
     <>
-      <div className="my-6 grid md:grid-cols-12 gap-6 md:gap-4 relative max-w-full">
+      <div className="my-6 grid md:grid-cols-12 lg:grid-cols-13 xl:grid-cols-14 gap-6 md:gap-4 relative max-w-full">
         <div className="md:col-span-full">
           <h1 className="text-3xl font-medium flex items-center gap-2">
             <Building2Icon className="size-8 flex-none text-grey" />
@@ -104,7 +104,7 @@ export default function SettingsLayoutPage({}: Route.ComponentProps) {
             </ul>
           </nav>
         </aside>
-        <div className="md:col-span-9 overflow-hidden py-1 px-2">
+        <div className="md:col-span-9 lg:col-span-10 xl:col-span-11 overflow-hidden py-1 px-2">
           <Outlet />
         </div>
       </div>
